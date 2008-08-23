04ac940795d7499357130e688e927e45abaae973
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2008-08-23 23:52:34 +0000

    Tag for first official release!!

---
+++ tags/0.5/0.5.1/MovingPictures/DataProviders/LocalProvider.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using MediaPortal.Plugins.MovingPictures.Database.MovingPicturesTables;
+using System.Text.RegularExpressions;
+using MediaPortal.Plugins.MovingPictures.Database;
+using NLog;
+using System.Net;
+
+namespace MediaPortal.Plugins.MovingPictures.DataProviders {
+    public class LocalProvider : IBackdropProvider {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        private DBMovieInfo movie;
+
+        public bool GetBackdrop(DBMovieInfo movie) {
+            this.movie = movie;
+
+            // if we already have a backdrop move on for now
+            if (movie.BackdropFullPath.Trim().Length > 0)
+                return false;
+
+            // grab our settings
+            string pattern = MovingPicturesCore.SettingsManager["local_backdrop_pattern"].StringValue;
+            string backdropFolderPath = MovingPicturesCore.SettingsManager["backdrop_folder"].StringValue;
+            
+            // try to create our filename(s)
+            Regex parser = new Regex("%(.*?)%", RegexOptions.IgnoreCase);
+            List<string> filenames = new List<string>();
+            foreach (string currPattern in pattern.Split('|')) {
+                filenames.Add(parser.Replace(currPattern, new MatchEvaluator(dbNameParser)).Trim().ToLower());
+            }
+
+            // grab the list of files in the backdrop folder and see if any match
+            DirectoryInfo backdropFolder = null;
+            try {
+                backdropFolder = new DirectoryInfo(backdropFolderPath);
+                FileInfo[] fileList = backdropFolder.GetFiles("*", SearchOption.TopDirectoryOnly);
+                foreach (FileInfo currFile in fileList) {
+                    foreach (string currFileName in filenames)
+                        if (currFile.Name.ToLower().Equals(currFileName)) {
+                            movie.BackdropFullPath = currFile.FullName;
+                            return true;
+                        }
+                }
+            }
+            catch (Exception e) {
+                logger.Error("Error scanning " + (backdropFolder == null ? "folder" : backdropFolder.FullName) + " for Backdrops.", e);
+            }
+
+            return false;
+        }
+
+        private string dbNameParser(Match match) {
+            // try to grab the field object
+            string fieldName = match.Value.Substring(1, match.Length - 2);
+            DBField field = DBField.GetFieldByDBName(typeof(DBMovieInfo), fieldName);
+            
+            // if no dice, the user probably entered an invalid string.
+            if (field == null) {
+                logger.Error("Error parsing \"" + match.Value + "\" from local_backdrop_pattern advanced setting. Not a database field name.");
+                return match.Value;
+            }
+
+            return field.GetValue(movie).ToString();
+        }
+
+    }
+}
