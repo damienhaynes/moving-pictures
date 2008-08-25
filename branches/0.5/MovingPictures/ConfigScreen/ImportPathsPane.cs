e8fabb6d79cfaeb022e057d0d0d050230e375350
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2008-08-25 00:44:21 +0000

    Branch for the 0.5 line of releases. ONLY BUG FIXES GO IN HERE. That means if there is not an issue in the issue tracker marked "Defect" matching a code change you're making, it shouldn't go here. Anything not paired to a "Defect" Issue should go in the trunk.

---
+++ branches/0.5/MovingPictures/ConfigScreen/ImportPathsPane.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+using MediaPortal.Plugins.MovingPictures.Database;
+using MediaPortal.Plugins.MovingPictures.Database.MovingPicturesTables;
+
+namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
+    public partial class ImportPathsPane : UserControl {
+        private List<DBImportPath> paths = new List<DBImportPath>();
+        private BindingSource pathBindingSource;
+        
+        public ImportPathsPane() {
+            InitializeComponent();
+
+            // if we are in designer, break to prevent errors with rendering, it cant access the DB...
+            if (LicenseManager.UsageMode == LicenseUsageMode.Designtime)
+                return;
+
+            // grab all existing paths
+            paths = DBImportPath.GetAll();
+
+            // set up the binding for the on screen control
+            pathBindingSource = new BindingSource();
+            pathBindingSource.DataSource = paths;
+            pathBindingSource.ListChanged += new ListChangedEventHandler(pathBindingSource_ListChanged);
+
+            // assign the bound list of paths to the control
+            pathsGridView.AutoGenerateColumns = false;
+            pathsGridView.DataSource = pathBindingSource;
+        }
+
+        // Commits new and existing itmes on addition or modification.
+        void pathBindingSource_ListChanged(object sender, ListChangedEventArgs e) {
+            if (e.ListChangedType != ListChangedType.ItemDeleted) {
+
+                DBImportPath changedObj = (DBImportPath)pathBindingSource[e.NewIndex];
+                changedObj.Commit();
+            }
+        }
+                
+        private void addSourceButton_Click(object sender, EventArgs e) {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            DialogResult result = folderDialog.ShowDialog();
+            if (result == DialogResult.OK) {
+                DBImportPath newPath = new DBImportPath();
+                newPath.FullPath = folderDialog.SelectedPath;
+                pathBindingSource.Add(newPath);
+                MovingPicturesCore.Importer.StartFullScan();
+            }
+            
+        }
+
+        private void removeSourceButton_Click(object sender, EventArgs e) {
+            if (pathBindingSource.Current != null) {
+                ((DBImportPath)pathBindingSource.Current).Delete();
+                pathBindingSource.RemoveCurrent();
+            }
+        }
+
+
+
+    }
+}
