3cbd0b1f30f1db21730b67640e776284fa803a43
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2008-09-11 04:08:20 +0000

    removing branch again. we need to establish some better standards for how we operate with branches, patches, etc.

--- branches/0.5.5/External/merge.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-IF EXIST MovingPictures_UNMERGED.dll del MovingPictures_UNMERGED.dll
-ren MovingPictures.dll MovingPictures_UNMERGED.dll
-ilmerge /out:MovingPictures.dll MovingPictures_UNMERGED.dll NLog.dll
