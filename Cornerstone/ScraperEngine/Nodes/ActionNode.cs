a6d521bafce28898d08fa8d062c1110117467050
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2008-09-14 19:19:21 +0000

    Issue #9: Additional work on the IMDb scraper, ad scraping engine. Fixed multi-threading bug causing weird behavior when doing a regular scan. Added a few math nodes.

--- Cornerstone/ScraperEngine/Nodes/ActionNode.cs
+++ Cornerstone/ScraperEngine/Nodes/ActionNode.cs
@@ -6,41 +6,16 @@ using System.Xml;
 namespace Cornerstone.ScraperEngine.Nodes {
     [ScraperNode("action")]
     public class ActionNode: ScraperNode {
-        #region Properties
 
-        public override Dictionary<string, string> InputVariables {
-            get {
-                return base.InputVariables;
-            }
-            set {
-                // for the action node we want to create a new global variable
-                // object and we dont want to modify the dictionary passed in
-                // by the calling class, so we override here.
-                inputVariables.Clear();
-                globalVariables.Clear();
-                foreach (KeyValuePair<string, string> currPair in value) {
-                    inputVariables[currPair.Key] = currPair.Value;
-                    globalVariables[currPair.Key] = currPair.Value;
-                }
-            }
-        }
-
-        #endregion
-
-        #region Methods
 
         public ActionNode(XmlNode xmlNode, bool debugMode)
             : base(xmlNode, debugMode) {
-        
-            
         }
 
-        public override void Execute() {
-            base.Execute();
-            executeChildren();
+        public override void Execute(Dictionary<string, string> variables) {
+            executeChildren(variables);
         }
 
-        #endregion
 
     }
 }
