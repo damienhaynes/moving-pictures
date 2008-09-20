8e034f59ddcdf8cb5a90505cd048c9f63db5224e
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2008-09-20 15:23:51 +0000

    Oops, forgot to check these in, first draft of the DataProviderManager.

---
+++ trunk/MovingPictures/DataProviders/DataProviderManager.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaPortal.Plugins.MovingPictures.Database;
+using Cornerstone.Database;
+using Cornerstone.Database.Tables;
+using MediaPortal.Plugins.MovingPictures.Properties;
+using System.Reflection;
+
+namespace MediaPortal.Plugins.MovingPictures.DataProviders {
+    public class DataProviderManager {
+        private static DataProviderManager instance = null;
+        private DatabaseManager dbManager = null;
+        
+        private List<DBSourceInfo> detailSources;
+        private List<DBSourceInfo> coverSources;
+        private List<DBSourceInfo> backdropSources;
+        private List<DBSourceInfo> allSources;
+
+        public static DataProviderManager GetInstance() {
+            if (instance == null)
+                instance = new DataProviderManager();
+
+            return instance;
+        }
+
+        private DataProviderManager() {
+            dbManager = MovingPicturesCore.DatabaseManager;
+            
+            detailSources = new List<DBSourceInfo>();
+            coverSources = new List<DBSourceInfo>();
+            backdropSources = new List<DBSourceInfo>();
+            allSources = new List<DBSourceInfo>();
+
+            loadProvidersFromDatabase();
+            loadMissingDefaultProviders();
+
+            //DBSetting alreadyInitedSetting = MovingPicturesCore.SettingsManager["internal_init_dp_manager_done"];
+            //bool alreadyInited = (bool) alreadyInitedSetting.Value;
+
+            //if (!alreadyInited) {
+            //    restoreToDefaults();
+            //    alreadyInitedSetting.Value = "true";
+            //    alreadyInitedSetting.Commit();
+            //}
+
+        }
+
+        private void loadProvidersFromDatabase() {
+            allSources = DBSourceInfo.GetAll();
+            foreach (DBSourceInfo currSource in allSources) 
+                add(currSource);
+        }
+
+        private void loadMissingDefaultProviders() {
+            foreach (Type currType in Assembly.GetExecutingAssembly().GetTypes()) {
+                if (!currType.IsClass)
+                    continue;
+
+                // check if the found type is a scriptable movie provider
+                bool found = false;
+                foreach (Type currInterface in currType.GetInterfaces())
+                    if (currInterface == typeof(IScriptableMovieProvider)) {
+                        found = true;
+
+                        IScriptableMovieProvider newProvider = (IScriptableMovieProvider) Activator.CreateInstance(currType);
+                        List<string> defaultScripts = newProvider.GetDefaultScripts();
+
+                        // TODO: build list of possible scraper engines here
+
+                        // load any default scripts if they are not already loaded.
+                        foreach (string script in defaultScripts) {
+                            DBScriptInfo scriptInfo = new DBScriptInfo();
+                            scriptInfo.Contents = script;
+                            
+                            bool alreadyLoaded = false;
+                            foreach (DBSourceInfo currSource in allSources)
+                                if (currSource.Scripts.Contains(scriptInfo)) {
+                                    alreadyLoaded = true; 
+                                    break;
+                                }
+
+                            if (!alreadyLoaded) {
+                                DBSourceInfo newSource = new DBSourceInfo();
+                                newSource.ProviderType = currType;
+                                newSource.Scripts.Add(scriptInfo);
+                                newSource.SelectedScript = scriptInfo;
+                                add(newSource);
+                            }
+                        }
+                    }
+
+                if (found) return;
+
+                // check if the found type is a regular movie provider
+                foreach (Type currInterface in currType.GetInterfaces())
+                    if (currInterface == typeof(IMovieProvider)) {
+                        bool alreadyLoaded = false;
+                        foreach (DBSourceInfo currSource in allSources) 
+                            if (currSource.ProviderType == currType)
+                                alreadyLoaded = true;
+
+                        if (alreadyLoaded) break;
+
+                        DBSourceInfo newSource = new DBSourceInfo();
+                        newSource.ProviderType = currType;
+                        add(newSource);
+                    }
+            }
+        }
+
+        private void add(DBSourceInfo newSource) {
+            allSources.Add(newSource);
+            if (newSource.Provider.ProvidesBackdrops)
+                backdropSources.Add(newSource);
+
+            if (newSource.Provider.ProvidesCoverArt)
+                coverSources.Add(newSource);
+
+            if (newSource.Provider.ProvidesMoviesDetails)
+                detailSources.Add(newSource);
+        }
+        
+        public List<DBMovieInfo> Get(string movieTitle) {
+            return detailSources[0].Provider.Get(movieTitle);
+        }
+
+        public void Update(DBMovieInfo movie) {
+            detailSources[0].Provider.Update(movie);            
+        }
+
+        public bool GetArtwork(DBMovieInfo movie) {
+            return coverSources[0].Provider.GetArtwork(movie);
+        }
+
+        public bool GetBackdrop(DBMovieInfo movie) {
+            foreach (DBSourceInfo currSource in backdropSources) {
+                bool success = currSource.Provider.GetBackdrop(movie);
+                if (success) return true; ;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ trunk/MovingPictures/Database/DBSourceInfo.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cornerstone.Database.Tables;
+using MediaPortal.Plugins.MovingPictures.DataProviders;
+using Cornerstone.Database;
+using Cornerstone.Database.CustomTypes;
+using NLog;
+
+namespace MediaPortal.Plugins.MovingPictures.Database {
+    [DBTableAttribute("source_info")]
+    public class DBSourceInfo: MovingPicturesDBTable {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        #region Database Fields
+        
+        [DBFieldAttribute]
+        public Type ProviderType {
+            get { return providerType; }
+            set {
+                providerType = value;
+                commitNeeded = true;
+            }
+        } protected Type providerType;
+
+
+        [DBFieldAttribute(Default="null")]
+        public DBScriptInfo SelectedScript {
+            get { return selectedScript; }
+            set {
+                selectedScript = value;
+                commitNeeded = true;
+            }
+        } protected DBScriptInfo selectedScript = null;
+
+        
+        [DBRelation(AutoRetrieve = true)]
+        public RelationList<DBSourceInfo, DBScriptInfo> Scripts {
+            get {
+                if (scripts == null) {
+                    scripts = new RelationList<DBSourceInfo, DBScriptInfo>(this);
+                }
+                return scripts;
+            }
+        } RelationList<DBSourceInfo, DBScriptInfo> scripts;
+
+        [DBFieldAttribute]
+        public int? DetailsPriority {
+            get { return detailsPriority; }
+            set {
+                detailsPriority = value;
+                commitNeeded = true;
+            }
+        } protected int? detailsPriority;
+
+        [DBFieldAttribute]
+        public int? CoverPriority {
+            get { return coverPriority; }
+            set {
+                coverPriority = value;
+                commitNeeded = true;
+            }
+        } protected int? coverPriority;
+
+        [DBFieldAttribute]
+        public int? BackdropPriority {
+            get { return backdropPriority; }
+            set {
+                backdropPriority = value;
+                commitNeeded = true;
+            }
+        } protected int? backdropPriority;
+
+        #endregion
+
+        #region Properties
+
+        // Friendly name for the script.
+        public string Name {
+            get { return Provider.Name; }
+        } 
+
+        public IMovieProvider Provider {
+            get {
+                if (provider == null) {
+                    provider = (IMovieProvider) Activator.CreateInstance(providerType);
+                    if (selectedScript != null && selectedScript.Contents.Trim().Length != 0)
+                        try {
+                            ((IScriptableMovieProvider)provider).Load(selectedScript.Contents);
+                        }
+                        catch (Exception) {
+                            logger.Warn("DataProvider tried to load a script when it doesn't support scripts!");
+                            return null;
+                        }
+                }
+
+                return provider;
+            }
+        } protected IMovieProvider provider = null;
+
+        #endregion
+
+        #region Static Methods
+
+        public static List<DBSourceInfo> GetAll() {
+            return MovingPicturesCore.DatabaseManager.Get<DBSourceInfo>(null);
+        }
+
+        #endregion
+
+    }
+}
