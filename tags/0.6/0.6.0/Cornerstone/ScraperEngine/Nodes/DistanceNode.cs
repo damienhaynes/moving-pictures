0bddf7601a30dcdb3d4990c649df27a4237f8ec9
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2008-11-21 16:15:25 +0000

    Tag for 0.6.0 Release

---
+++ tags/0.6/0.6.0/Cornerstone/ScraperEngine/Nodes/DistanceNode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cornerstone.Tools;
+using System.Xml;
+using System.Threading;
+
+namespace Cornerstone.ScraperEngine.Nodes {
+    [ScraperNode("distance")]
+    public class DistanceNode : ScraperNode {
+        public string String1 {
+            get { return string1; }
+        } protected string string1;
+
+        public string String2 {
+            get { return string2; }
+        } protected string string2;
+
+        public DistanceNode(XmlNode xmlNode, bool debugMode)
+            : base(xmlNode, debugMode) {
+
+            // try to grab the first value
+            try { string1 = xmlNode.Attributes["string1"].Value; }
+            catch (Exception e) {
+                if (e.GetType() == typeof(ThreadAbortException))
+                    throw e;
+
+                logger.Error("Missing STRING1 attribute on: " + xmlNode.OuterXml);
+                loadSuccess = false;
+                return;
+            }
+
+            // try to grab the second value
+            try { string2 = xmlNode.Attributes["string2"].Value; }
+            catch (Exception e) {
+                if (e.GetType() == typeof(ThreadAbortException))
+                    throw e;
+
+                logger.Error("Missing STRING2 attribute on: " + xmlNode.OuterXml);
+                loadSuccess = false;
+                return;
+            }
+        }
+
+        public override void Execute(Dictionary<string, string> variables) {
+            if (DebugMode) logger.Debug("executing distance: " + xmlNode.OuterXml);
+
+            string parsedString1 = parseString(variables, string1);
+            string parsedString2 = parseString(variables, string2);
+            if (DebugMode) logger.Debug("executing distance: " + parsedString1 + " vs. " + parsedString2);
+
+            int distance = AdvancedStringComparer.Levenshtein(parsedString1, parsedString2);
+
+            setVariable(variables, parseString(variables, Name), distance.ToString());
+        }
+
+    }
+}
---
+++ tags/0.6/0.6.0/MovingPictures/MainUI/MovieBrowser/RemoteNumpadFilter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaPortal.Plugins.MovingPictures.Database;
+using System.Text.RegularExpressions;
+using NLog;
+
+namespace MediaPortal.Plugins.MovingPictures.MainUI.MovieBrowser {
+    public class RemoteNumpadFilter : IBrowserFilter {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        
+        public event FilterUpdatedDelegate Updated;
+
+        private string _listFilterString; // List Filter String
+
+        public RemoteNumpadFilter() {
+            _listFilterString = string.Empty;
+        }
+
+        public List<DBMovieInfo> Filter(List<DBMovieInfo> input) {
+            // If the listFilterString contains characters
+            // filter the list using the current filter string
+            if (Active) {
+                logger.Debug("List Filter Active: '{0}'", _listFilterString);
+
+                // This is the "contains" predicate
+                // @todo: StartsWith, EndsWith etc.. ?
+                Predicate<DBMovieInfo> predicate = delegate(DBMovieInfo item) {
+                    return (NumPadEncode(item.Title).Contains(_listFilterString));
+                };
+                
+                // Filter the list with the specified critera
+                List<DBMovieInfo> filteredList = input.FindAll(predicate);
+                return filteredList;
+            }
+            else {
+                // fill facade (facade) with all items.
+                return input;
+            }
+        }
+
+        public bool Active {
+            get { return !String.IsNullOrEmpty(_listFilterString); }
+        }
+
+        public void Clear() {
+            _listFilterString = string.Empty;
+            
+            if (Updated != null)
+                Updated(this);
+        }
+
+        public bool KeyPress(Char key) {
+            if ((key >= '0' && key <= '9') || 
+                key == '*' || key == '(' || key == '#' || key == '§') {
+
+                // reset the list filter string
+                if (key == '*') 
+                    _listFilterString = string.Empty;
+                
+                // or add the numeric code to the list filter string
+                else 
+                    _listFilterString += NumPadEncode(key.ToString());
+
+                if (Updated != null)
+                    Updated(this);
+
+
+                logger.Debug("Active Filter: " + _listFilterString);
+                return true;
+
+            }
+            return false;
+        }
+    
+
+        public static string NumPadEncode(string input) {
+            string rtn = input.Trim();
+            rtn = Regex.Replace(rtn, @"[abc]", "2", RegexOptions.IgnoreCase);
+            rtn = Regex.Replace(rtn, @"[def]", "3", RegexOptions.IgnoreCase);
+            rtn = Regex.Replace(rtn, @"[ghi]", "4", RegexOptions.IgnoreCase);
+            rtn = Regex.Replace(rtn, @"[jkl]", "5", RegexOptions.IgnoreCase);
+            rtn = Regex.Replace(rtn, @"[mno]", "6", RegexOptions.IgnoreCase);
+            rtn = Regex.Replace(rtn, @"[pqrs]", "7", RegexOptions.IgnoreCase);
+            rtn = Regex.Replace(rtn, @"[tuv]", "8", RegexOptions.IgnoreCase);
+            rtn = Regex.Replace(rtn, @"[wxyz]", "9", RegexOptions.IgnoreCase);
+            rtn = Regex.Replace(rtn, @"[\s]", "0", RegexOptions.IgnoreCase);
+            return rtn;
+        }
+
+    }
+}
