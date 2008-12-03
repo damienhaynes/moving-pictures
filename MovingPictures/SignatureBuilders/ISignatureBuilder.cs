a041a1d8912f71fd8f00d87e8f69fd1e5d98a3e0
apondman <apondman@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2008-12-03 11:20:07 +0000

    Refactor: sorted and removed unused usings (no functional changes)

--- MovingPictures/SignatureBuilders/ISignatureBuilder.cs
+++ MovingPictures/SignatureBuilders/ISignatureBuilder.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using MediaPortal.Plugins.MovingPictures.LocalMediaManagement;
-using MediaPortal.Plugins.MovingPictures.Database;
 
 namespace MediaPortal.Plugins.MovingPictures.SignatureBuilders {
     public interface ISignatureBuilder {
