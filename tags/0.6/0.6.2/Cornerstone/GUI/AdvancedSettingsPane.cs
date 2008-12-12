a318a13374d3767eb9f12ee9e44b167fb86bbe33
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2008-12-12 23:17:36 +0000

    Tag for 0.6.2 release.

---
+++ tags/0.6/0.6.2/Cornerstone/GUI/AdvancedSettingsPane.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using Cornerstone.Database;
+using Cornerstone.Database.Tables;
+
+namespace Cornerstone.GUI {
+    public partial class AdvancedSettingsPane : UserControl {
+        private SettingsManager settings;
+        private DBSetting selectedSetting;
+        private Color validColor;
+        private Color invalidColor;
+        Dictionary<string, TreeNode> groups = new Dictionary<string, TreeNode>();
+        
+        public AdvancedSettingsPane() {
+            InitializeComponent();
+
+            invalidColor = Color.Red;
+            validColor = setValueTextBox.ForeColor;
+            // if we are in designer, break to prevent errors with rendering, it cant access the DB...
+            if (LicenseManager.UsageMode == LicenseUsageMode.Designtime)
+                return;
+        }
+
+        public void populateTree(SettingsManager settings) {
+            this.settings = settings;
+
+            // stop redrawing and clear tree
+            advancedSettingsTreeView.BeginUpdate();
+            advancedSettingsTreeView.Nodes.Clear();
+
+            // loop through all settings and add nodes for groups and settings accordingly.
+            // maintain a dictionary of groups to ensure on one listing per group.
+            foreach (DBSetting currSetting in settings.Values) {
+                string groupKey = "";
+                TreeNode parentNode = null;
+
+                // loop through all parent groups of this setting and if no node has been made for any
+                // of them yet, go ahead and make a node
+                foreach (string currGroup in currSetting.Grouping) {
+                    groupKey += currGroup + '|';
+                    if (!groups.ContainsKey(groupKey)) {
+                        // create new node and place in TreeView
+                        TreeNode newNode = new TreeNode(currGroup);
+                        if (parentNode == null)
+                            advancedSettingsTreeView.Nodes.Add(newNode);
+                        else
+                            parentNode.Nodes.Add(newNode);
+
+                        newNode.BackColor = Color.LightGray;
+                        newNode.NodeFont = new Font(this.Font.Name, this.Font.Size,
+                                                    this.Font.Style, this.Font.Unit);
+
+                        // update parentNode var (*since we arer walking up group chain), 
+                        // and store node in our dictionary.
+                        parentNode = newNode;
+                        groups.Add(groupKey, newNode);
+                    } else
+                        parentNode = groups[groupKey];
+                }
+
+                // create a node for this setting an place it under it's parent group
+                TreeNode settingNode = new TreeNode(currSetting.Name);
+                settingNode.Tag = currSetting;
+                
+                if (parentNode == null)
+                    advancedSettingsTreeView.Nodes.Add(settingNode);
+                else
+                    parentNode.Nodes.Add(settingNode);
+            }
+
+            // restrart drawing
+            advancedSettingsTreeView.ExpandAll();
+            advancedSettingsTreeView.EndUpdate();
+            
+        }
+
+        // if the user single clicks on an item, show the value of that item
+        private void advancedSettingsTreeView_AfterSelect(object sender, System.Windows.Forms.TreeViewEventArgs e) {           
+            TreeNode selectedNode = advancedSettingsTreeView.SelectedNode;                        
+            // if the user selected a settings node
+            if (selectedNode != null &&
+                selectedNode.Tag != null &&
+                selectedNode.Tag.GetType() == typeof(DBSetting)) 
+            {
+
+                setValueTextBox.Enabled = true;
+                selectedSetting = (DBSetting)selectedNode.Tag;
+                setValueTextBox.Text = selectedSetting.StringValue;
+                setDescriptionLabel.Text = selectedSetting.Description;
+            } 
+            else {
+                setValueTextBox.Enabled = false;
+                setDescriptionLabel.Text = string.Empty;
+                setValueTextBox.Text = String.Empty;
+            }
+
+            updateSettingButton.Enabled = false;
+        }
+
+        // if the value is valid and the user hits enter, go ahead and save the value.
+        void setValueTextBox_KeyUp(object sender, System.Windows.Forms.KeyEventArgs e) {
+            if (e.KeyValue == 13 && valueIsValid()) 
+                commitSetting();
+        }
+
+        // returns true if the value in the text box is a valid value for the setting
+        // represented by this popup.
+        private bool valueIsValid() {
+            return selectedSetting.Validate(setValueTextBox.Text);
+        }
+
+        private void setValueTextBox_TextChanged(object sender, EventArgs e) {
+            if (valueIsValid()) {
+                setValueTextBox.ForeColor = validColor;
+                updateSettingButton.Enabled = true;
+            } else {
+                setValueTextBox.ForeColor = invalidColor;
+                updateSettingButton.Enabled = false;
+            }
+        }
+
+        private void updateSettingButton_Click(object sender, EventArgs e) {
+            if (valueIsValid()) 
+                commitSetting();
+        }
+
+        private void commitSetting() {
+            selectedSetting.Value = setValueTextBox.Text;
+            selectedSetting.Commit();
+            updateSettingButton.Enabled = false;
+        }
+    }
+}
---
+++ tags/0.6/0.6.2/MovingPictures/ConfigScreen/MovieImporterPane.cs
@@ -0,0 +1,403 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+using MediaPortal.Plugins.MovingPictures.LocalMediaManagement;
+using MediaPortal.Plugins.MovingPictures.Database;
+using MediaPortal.Plugins.MovingPictures.Properties;
+using MediaPortal.Plugins.MovingPictures.ConfigScreen.Popups;
+using NLog;
+
+
+namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
+    public partial class MovieImporterPane : UserControl {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        private Bitmap blank;
+        private bool splitMode;
+        private int lastSplitJoinLocation;
+        private bool clearSelection;
+
+        #region Database Managed Settings
+        
+        #endregion
+
+        public MovieImporterPane() {
+            InitializeComponent();
+
+            blank = new Bitmap(1, 1);
+            splitMode = true;
+            clearSelection = false;
+
+            // if we are in designer, break to prevent errors with rendering, it cant access the DB...
+            if (LicenseManager.UsageMode == LicenseUsageMode.Designtime)
+                return;
+        }
+
+        private void progressListener(int percentDone, int taskCount, int taskTotal, string taskDescription) {
+            // This ensures we are thread safe. Makes sure this method is run by
+            // the thread that created this panel.
+            if (InvokeRequired) {
+                Invoke(new MovieImporter.ImportProgressHandler(progressListener), new object[] { percentDone, taskCount, taskTotal, taskDescription });
+                return;
+            }
+            
+            // set visibility of progressbar and info labels
+            currentTaskDesc.Visible = true;
+            countProgressLabel.Visible = true;
+
+            // set the values of the progress bar and info labels
+            progressBar.Value = percentDone;
+            currentTaskDesc.Text = taskDescription;
+            countProgressLabel.Text = "(" + taskCount + "/" + taskTotal + ")";
+
+            // if finished hide the progress info components
+            if (percentDone == 100) {
+                currentTaskDesc.Visible = false;
+                countProgressLabel.Visible = false;
+                progressBar.Value = 0;
+            }
+        }
+
+        private void movieStatusChangedListener(MovieMatch obj, MovieImporterAction action) {
+            // This ensures we are thread safe. Makes sure this method is run by
+            // the thread that created this panel.
+            if (InvokeRequired) {
+                Delegate method = new MovieImporter.MovieStatusChangedHandler(movieStatusChangedListener);
+                object[] parameters = new object[] { obj, action };
+                this.Invoke(method, parameters);
+                return;
+            }
+
+            if (action == MovieImporterAction.STARTED)
+                return;
+
+            if (action == MovieImporterAction.STOPPED) {
+                unapprovedMatchesBindingSource.Clear();
+                return;
+            }
+
+            if (action == MovieImporterAction.REMOVED_FROM_SPLIT ||
+                action == MovieImporterAction.REMOVED_FROM_JOIN) {
+
+                lastSplitJoinLocation = unapprovedMatchesBindingSource.IndexOf(obj);
+                unapprovedMatchesBindingSource.Remove(obj);
+                clearSelection = true;
+                return;
+            }
+
+
+            // add the match if neccisary and grab the row number
+            int rowNum;
+            if (action == MovieImporterAction.ADDED)
+                rowNum = unapprovedMatchesBindingSource.Add(obj);
+            else if (action == MovieImporterAction.ADDED_FROM_SPLIT ||
+                     action == MovieImporterAction.ADDED_FROM_JOIN) {
+
+                unapprovedMatchesBindingSource.Insert(lastSplitJoinLocation, obj);
+                if (clearSelection) {
+                    unapprovedGrid.ClearSelection();
+                    clearSelection = false;
+                }
+                unapprovedGrid.Rows[lastSplitJoinLocation].Selected = true;
+
+                rowNum = lastSplitJoinLocation;
+                lastSplitJoinLocation++;
+            } else 
+                rowNum = unapprovedMatchesBindingSource.IndexOf(obj);
+            
+
+            // setup tooltip for filename
+            DataGridViewTextBoxCell filenameCell = (DataGridViewTextBoxCell)unapprovedGrid.Rows[rowNum].Cells["unapprovedLocalMediaColumn"];
+            filenameCell.ToolTipText = obj.LongLocalMediaString;
+
+            // setup the combo box of possible matches
+            DataGridViewComboBoxCell movieListCombo = (DataGridViewComboBoxCell)unapprovedGrid.Rows[rowNum].Cells["unapprovedPossibleMatchesColumn"];
+            movieListCombo.Items.Clear();
+            foreach (PossibleMatch currMatch in obj.PossibleMatches)
+                movieListCombo.Items.Add(currMatch);
+             
+            // set the status icon
+            DataGridViewImageCell imageCell = (DataGridViewImageCell)unapprovedGrid.Rows[rowNum].Cells["statusColumn"];
+            switch (action) {
+                case MovieImporterAction.ADDED:
+                case MovieImporterAction.ADDED_FROM_SPLIT:
+                case MovieImporterAction.ADDED_FROM_JOIN:
+                    imageCell.Value = blank;
+                    break;
+                case MovieImporterAction.PENDING:
+                    imageCell.Value = Resources.arrow_rotate_clockwise;
+                    break;
+                case MovieImporterAction.GETTING_MATCHES:
+                    imageCell.Value = Resources.arrow_down1;
+                    break;
+                case MovieImporterAction.NEED_INPUT:
+                    imageCell.Value = Resources.information;
+                    break;
+                case MovieImporterAction.APPROVED:
+                    imageCell.Value = Resources.approved;
+                    break;
+                case MovieImporterAction.GETTING_DETAILS:
+                    imageCell.Value = Resources.approved;
+                    break;
+                case MovieImporterAction.COMMITED:
+                    imageCell.Value = Resources.accept;
+                    break;
+                case MovieImporterAction.IGNORED:
+                    imageCell.Value = Resources.ignored;
+                    break;
+                case MovieImporterAction.MANUAL:
+                    imageCell.Value = Resources.accept; // @TODO change icon
+                    break;
+            }
+
+            updateButtons();
+        }
+
+        private void scanButton_Click(object sender, EventArgs e) {
+            MovingPicturesCore.Importer.Start();
+            MovingPicturesCore.Importer.RestartScanner();
+        }
+
+        private void MovieImporterPane_Load(object sender, EventArgs e) {
+            if (!DesignMode) {
+                MovingPicturesCore.Importer.Progress += new MovieImporter.ImportProgressHandler(progressListener);
+                MovingPicturesCore.Importer.MovieStatusChanged += new MovieImporter.MovieStatusChangedHandler(movieStatusChangedListener);
+
+                // the importer needs to be displayed on load, however briefly, to get things 
+                // initialized. It is currently the first tab so this is not needed right now.
+                // tabControl.SelectedIndex = 1;
+                // tabControl.SelectedIndex = 0;
+
+                MovingPicturesCore.Importer.Start();
+                
+                updateButtons();
+            }
+
+            this.unapprovedGrid.AutoGenerateColumns = false;
+            this.unapprovedGrid.DataSource = this.unapprovedMatchesBindingSource;
+
+            this.unapprovedPossibleMatchesColumn.DisplayMember = "DisplayMember";
+            this.unapprovedPossibleMatchesColumn.ValueMember = "ValueMember";
+        }
+
+
+        // If this pane has been destroyed, that means the Config screen has shut down, so 
+        // the importer also should be  stopped.
+        void MovieImporterPane_HandleDestroyed(object sender, System.EventArgs e) {
+            if (!DesignMode)
+                MovingPicturesCore.Importer.Stop();
+        }
+
+        // Handles when the user modifies data in the unapproved matches list
+        private void unapprovedMatchesBindingSource_ListChanged(object sender, ListChangedEventArgs e) {
+            if (e.ListChangedType == ListChangedType.ItemChanged) {
+                MovieMatch match = (MovieMatch)unapprovedGrid.Rows[e.NewIndex].DataBoundItem;
+                MovingPicturesCore.Importer.Approve(match);
+            }
+        }
+
+        private void approveButton_Click(object sender, EventArgs e) {
+            unapprovedGrid.EndEdit();
+
+            foreach (DataGridViewRow currRow in unapprovedGrid.SelectedRows) {
+                MovieMatch selectedMatch = (MovieMatch)currRow.DataBoundItem;
+                selectedMatch.HighPriority = true;
+                MovingPicturesCore.Importer.Approve(selectedMatch);
+            }
+        }
+
+        private void ignoreButton_Click(object sender, EventArgs e) {
+            unapprovedGrid.EndEdit();
+
+            DialogResult result = MessageBox.Show("This will permanently ignore the selected file(s). This action is currently IRREVERSABLE on a file by file basis, are you sure?", "Warning!", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes) {
+
+                foreach (DataGridViewRow currRow in unapprovedGrid.SelectedRows) {
+                    MovieMatch selectedMatch = (MovieMatch)currRow.DataBoundItem;
+                    MovingPicturesCore.Importer.Ignore(selectedMatch);
+                }
+            }
+        }
+
+        private void rescanButton_Click(object sender, EventArgs e) {
+            unapprovedGrid.EndEdit();
+
+            foreach (DataGridViewRow currRow in unapprovedGrid.SelectedRows) {
+                MovieMatch selectedMatch = (MovieMatch)currRow.DataBoundItem;
+
+                SearchStringPopup popup = new SearchStringPopup(selectedMatch);
+                popup.ShowDialog(this);
+
+                if (popup.DialogResult == DialogResult.OK) {                
+                    // get the system generated signature
+                    MovieSignature sig = selectedMatch.Signature;
+                    // update it with the user supplied search string
+                    // TODO: make the user input box reflect the signature properties
+                    sig.Title = popup.GetSearchString();
+                    sig.Year = 0;
+                    sig.ImdbId = string.Empty;
+                    // add the user input based signature to the match
+                    selectedMatch.Signature = sig;
+                    // reprocess
+                    MovingPicturesCore.Importer.Reprocess(selectedMatch);
+                }
+            }
+        }
+
+        private void unapprovedGrid_SelectionChanged(object sender, EventArgs e) {
+            updateButtons();
+        }
+
+        // updates enabled/disabled status of buttons on media importer based on 
+        // the rows selected in the list
+        private void updateButtons() {
+            bool approveButtonEnabled = false;
+            bool validMatchSelected = false;
+            bool ignoreButtonEnabled = false;
+
+            foreach (DataGridViewRow currRow in unapprovedGrid.SelectedRows) {
+                MovieMatch selectedMatch = (MovieMatch)currRow.DataBoundItem;
+
+                if (selectedMatch == null)
+                    break;
+
+                validMatchSelected = true;
+
+                // check if this row is either approved or commited
+                if (!approveButtonEnabled &&
+                    !MovingPicturesCore.Importer.ApprovedMatches.Contains(selectedMatch) &&
+                    !MovingPicturesCore.Importer.RetrievingDetailsMatches.Contains(selectedMatch) &&
+                    !MovingPicturesCore.Importer.CommitedMatches.Contains(selectedMatch) &&
+                    selectedMatch.Selected != null) {
+
+                    approveButtonEnabled = true;
+                }
+
+                if (!selectedMatch.LocalMedia[0].Ignored)
+                    ignoreButtonEnabled = true;
+            }
+
+            // if the selection has changed, enable the approve button
+            approveButton.Enabled = approveButtonEnabled;
+            rescanButton.Enabled = validMatchSelected;
+            ignoreButton.Enabled = ignoreButtonEnabled;
+
+            // check if we have multiple rows selected to join
+            if (unapprovedGrid.SelectedRows.Count > 1) {
+                splitJoinButton.Image = Resources.arrow_join;
+                splitJoinButton.ToolTipText = "Join Selected Files";
+                splitJoinButton.Enabled = true;
+                splitMode = false;
+            }
+
+            // check if we have one row with multiple files we can split
+            else if (unapprovedGrid.SelectedRows.Count == 1 && unapprovedGrid.SelectedRows[0] != null) {
+                MovieMatch match = (MovieMatch)unapprovedGrid.SelectedRows[0].DataBoundItem;
+                if (match.LocalMedia.Count > 1) {
+                    splitJoinButton.Image = Resources.arrow_divide;
+                    splitJoinButton.ToolTipText = "Split Selected File Group";
+                    splitJoinButton.Enabled = true;
+                    splitMode = true;
+                } else 
+                    splitJoinButton.Enabled = false;
+            }
+
+            // split join button cant be used now, so disable it.
+            else splitJoinButton.Enabled = false;
+
+        }
+
+        private void splitJoinButton_Click(object sender, EventArgs e) {
+            if (splitMode) {
+                MovieMatch match = (MovieMatch)unapprovedGrid.SelectedRows[0].DataBoundItem;
+                MovingPicturesCore.Importer.Split(match);
+            } else {
+                List<MovieMatch> mediaList = new List<MovieMatch>();
+                foreach (DataGridViewRow currRow in unapprovedGrid.SelectedRows) 
+                    mediaList.Add((MovieMatch)currRow.DataBoundItem);
+
+                MovingPicturesCore.Importer.Join(mediaList);
+            }
+            
+        }
+
+        private void unapprovedGrid_DataError(object sender, DataGridViewDataErrorEventArgs e) {
+            logger.WarnException("Error from Importer DataGrid.", e.Exception);
+        }
+
+        private void unignoreAllFilesToolStripMenuItem_Click(object sender, EventArgs e) {
+            DialogResult result =  MessageBox.Show(
+                "This will unignore ALL previously ignored files, and restart\n" +
+                "the Importer. This means all uncommitted matches from this\n" +
+                "import session will have to be reapproved. Do you want to\n" +
+                "continue?\n", "Warning", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes) {
+                ProgressPopup popup = new ProgressPopup(new ProgressPopup.WorkerDelegate(unignoreAllFiles));
+                popup.Owner = this.ParentForm;
+                popup.Show();
+            }
+           
+        }
+
+        private void unignoreAllFiles() {
+            foreach (DBLocalMedia currFile in DBLocalMedia.GetAll())
+                if (currFile.Ignored)
+                    currFile.Delete();
+
+            MovingPicturesCore.Importer.RestartScanner();
+        }
+
+        private void restartImporterToolStripMenuItem_Click(object sender, EventArgs e) {
+            DialogResult result = MessageBox.Show(
+                "You are about to restart the Movie Importer. This means that\n" +
+                "all uncommitted matches from this import session will have to\n" +
+                "be reapproved. Do you want to continue?\n", "Warning", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes) 
+                MovingPicturesCore.Importer.RestartScanner();
+
+        }
+
+        private void settingsButton_ButtonClick(object sender, EventArgs e) {
+            settingsButton.ShowDropDown();
+        }
+
+        private void manualAssignButton_Click(object sender, EventArgs e) {
+            unapprovedGrid.EndEdit();
+
+            foreach (DataGridViewRow currRow in unapprovedGrid.SelectedRows) {
+                MovieMatch selectedMatch = (MovieMatch)currRow.DataBoundItem;
+                ManualAssignPopup popup = new ManualAssignPopup(selectedMatch);
+                popup.ShowDialog(this);
+
+                if (popup.DialogResult == DialogResult.OK) {
+
+                    // create a movie with the user supplied information
+                    DBMovieInfo movie = new DBMovieInfo();
+                    movie.Title = popup.Title;
+                    movie.Year = popup.Year.GetValueOrDefault(0);
+
+                    // update the match
+                    PossibleMatch selectedMovie = new PossibleMatch();
+                    selectedMovie.Movie = movie;
+                    selectedMovie.MatchValue = 0;
+                    
+                    selectedMatch.PossibleMatches.Add(selectedMovie);
+                    selectedMatch.Selected = selectedMovie;
+
+             
+                    // Manually Assign Movie
+                    MovingPicturesCore.Importer.ManualAssign(selectedMatch);
+                }
+            }
+        }
+
+    }
+}
