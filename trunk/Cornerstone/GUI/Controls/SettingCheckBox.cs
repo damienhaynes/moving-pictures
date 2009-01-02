55b2f026d7ed622564c2cdec50fd3e7e990aa62e
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2009-01-02 23:53:30 +0000

    * Cleaned up the ImportPathsPane to look a little better and vista and generally have less clutter. The help popup box will be replaced with properly formatted HTML help in the next day or so.
    * Added a SettingsCheckBox class to use for working with bool based settings.

---
+++ trunk/Cornerstone/GUI/Controls/SettingCheckBox.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Cornerstone.Database.Tables;
+
+namespace Cornerstone.GUI.Controls {
+    public class SettingCheckBox: CheckBox {
+        public DBSetting Setting {
+            get { return _setting; }
+            set {
+                if (value == null)
+                    return;
+
+                if (!value.Type.Equals("BOOL"))
+                    throw new Exception("Invalid Setting Type, SettingCheckBox requires a bool setting.");
+
+                _setting = value;
+
+                // Update the control to reflect the setting
+                Text = _setting.Name;
+                Checked = (bool)_setting.Value;
+           }
+        }
+        private DBSetting _setting;
+
+        public SettingCheckBox() {
+            CheckedChanged += new EventHandler(SettingCheckBox_CheckedChanged);
+        }
+
+        void SettingCheckBox_CheckedChanged(object sender, EventArgs e) {
+            _setting.Value = Checked;
+            _setting.Commit();
+        }
+
+        
+    }
+}
