238a440e797d2ab3aa3198ce2f972936f2eb35c2
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2009-01-03 20:19:37 +0000

    * Fixed issue causing incorrect movie details to be retrieved after a (previously) correct match had been approved either by the user or the system. For example, use the custom search string "Chaos". It will correctly auto-approve the 2005 film but then pull details for "Spin City".

--- MovingPictures/Properties/AssemblyInfo.cs
+++ MovingPictures/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.6.4.389")]
+[assembly: AssemblyVersion("0.6.4.390")]
 //[assembly: AssemblyFileVersion("0.0.0.0")]
