0ca298b374533b55e3141b02f3ca92e990f8ed76
apondman <apondman@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2009-01-08 11:30:40 +0000

    Follow-up r410: MyMovies.it is now loaded in DataManager.

--- MovingPictures/DataProviders/DataProviderManager.cs
+++ MovingPictures/DataProviders/DataProviderManager.cs
@@ -232,7 +232,8 @@ namespace MediaPortal.Plugins.MovingPictures.DataProviders {
             AddSource(typeof(TheMovieDbProvider));
             AddSource(typeof(ScriptableProvider), Resources.Script_MovieMeter);
             AddSource(typeof(ScriptableProvider), Resources.Script_OFDb);
-            AddSource(typeof(ScriptableProvider), Resources.Script_Allocine);       
+            AddSource(typeof(ScriptableProvider), Resources.Script_Allocine);
+            AddSource(typeof(ScriptableProvider), Resources.Script_MyMoviesItalian);  
             AddSource(typeof(ScriptableProvider), Resources.Script_MoTechPosters);
             AddSource(typeof(ScriptableProvider), Resources.Script_IMPAwards);          
             
