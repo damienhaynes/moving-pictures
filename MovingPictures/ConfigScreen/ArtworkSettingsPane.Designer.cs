e003a0d6b8ad7050a348cb8c9e8c543c644aff99
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2009-01-08 05:59:47 +0000

    * Additional work on config screen overhaul. Settings tabs I think are complete, perhaps excluding a few minor tweaks here or there. Still need to clean up the importer panel and create the about tab.

--- MovingPictures/ConfigScreen/ArtworkSettingsPane.Designer.cs
+++ MovingPictures/ConfigScreen/ArtworkSettingsPane.Designer.cs
@@ -23,8 +23,6 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.groupBox = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.coverDataSources = new System.Windows.Forms.Button();
             this.backdropSourcesButton = new System.Windows.Forms.Button();
             this.coverCountTextBox = new Cornerstone.GUI.Controls.SettingsTextBox();
@@ -33,47 +31,15 @@
             this.backdropPatternTextBox = new Cornerstone.GUI.Controls.SettingsTextBox();
             this.coverPatternTextBox = new Cornerstone.GUI.Controls.SettingsTextBox();
             this.backdropFromMovieFolderCheckBox = new Cornerstone.GUI.Controls.SettingCheckBox();
-            this.helpButton1 = new System.Windows.Forms.Button();
             this.coverFromMovieFolderCheckBox = new Cornerstone.GUI.Controls.SettingCheckBox();
-            this.groupBox.SuspendLayout();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
-            // 
-            // groupBox
-            // 
-            this.groupBox.Controls.Add(this.groupBox2);
-            this.groupBox.Controls.Add(this.coverDataSources);
-            this.groupBox.Controls.Add(this.backdropSourcesButton);
-            this.groupBox.Controls.Add(this.coverCountTextBox);
-            this.groupBox.Controls.Add(this.groupBox1);
-            this.groupBox.Controls.Add(this.label1);
-            this.groupBox.Controls.Add(this.backdropPatternTextBox);
-            this.groupBox.Controls.Add(this.coverPatternTextBox);
-            this.groupBox.Controls.Add(this.backdropFromMovieFolderCheckBox);
-            this.groupBox.Controls.Add(this.helpButton1);
-            this.groupBox.Controls.Add(this.coverFromMovieFolderCheckBox);
-            this.groupBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox.Location = new System.Drawing.Point(0, 0);
-            this.groupBox.MinimumSize = new System.Drawing.Size(400, 147);
-            this.groupBox.Name = "groupBox";
-            this.groupBox.Size = new System.Drawing.Size(576, 147);
-            this.groupBox.TabIndex = 0;
-            this.groupBox.TabStop = false;
-            this.groupBox.Text = "Artwork";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox2.Location = new System.Drawing.Point(12, 104);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(552, 3);
-            this.groupBox2.TabIndex = 8;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
             // 
             // coverDataSources
             // 
-            this.coverDataSources.Location = new System.Drawing.Point(6, 113);
+            this.coverDataSources.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coverDataSources.Location = new System.Drawing.Point(127, 97);
             this.coverDataSources.Name = "coverDataSources";
             this.coverDataSources.Size = new System.Drawing.Size(131, 23);
             this.coverDataSources.TabIndex = 9;
@@ -83,7 +49,8 @@
             // 
             // backdropSourcesButton
             // 
-            this.backdropSourcesButton.Location = new System.Drawing.Point(143, 113);
+            this.backdropSourcesButton.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backdropSourcesButton.Location = new System.Drawing.Point(264, 97);
             this.backdropSourcesButton.Name = "backdropSourcesButton";
             this.backdropSourcesButton.Size = new System.Drawing.Size(139, 23);
             this.backdropSourcesButton.TabIndex = 8;
@@ -93,19 +60,20 @@
             // 
             // coverCountTextBox
             // 
-            this.coverCountTextBox.Location = new System.Drawing.Point(275, 17);
+            this.coverCountTextBox.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coverCountTextBox.Location = new System.Drawing.Point(397, 3);
             this.coverCountTextBox.Name = "coverCountTextBox";
             this.coverCountTextBox.Setting = null;
-            this.coverCountTextBox.Size = new System.Drawing.Size(24, 20);
+            this.coverCountTextBox.Size = new System.Drawing.Size(24, 21);
             this.coverCountTextBox.TabIndex = 5;
             // 
             // groupBox1
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 43);
+            this.groupBox1.Location = new System.Drawing.Point(120, 27);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(552, 3);
+            this.groupBox1.Size = new System.Drawing.Size(424, 3);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
@@ -113,98 +81,120 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 6);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(255, 13);
+            this.label1.Size = new System.Drawing.Size(254, 13);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Maximum Number of Covers to Download per Movie:";
+            this.label1.Text = "Maximum number of covers to download per movie:";
             // 
             // backdropPatternTextBox
             // 
             this.backdropPatternTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.backdropPatternTextBox.Location = new System.Drawing.Point(243, 78);
+            this.backdropPatternTextBox.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backdropPatternTextBox.Location = new System.Drawing.Point(379, 62);
             this.backdropPatternTextBox.Name = "backdropPatternTextBox";
             this.backdropPatternTextBox.Setting = null;
-            this.backdropPatternTextBox.Size = new System.Drawing.Size(327, 20);
+            this.backdropPatternTextBox.Size = new System.Drawing.Size(163, 21);
             this.backdropPatternTextBox.TabIndex = 3;
             // 
             // coverPatternTextBox
             // 
             this.coverPatternTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.coverPatternTextBox.Location = new System.Drawing.Point(243, 52);
+            this.coverPatternTextBox.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coverPatternTextBox.Location = new System.Drawing.Point(379, 36);
             this.coverPatternTextBox.Name = "coverPatternTextBox";
             this.coverPatternTextBox.Setting = null;
-            this.coverPatternTextBox.Size = new System.Drawing.Size(327, 20);
+            this.coverPatternTextBox.Size = new System.Drawing.Size(163, 21);
             this.coverPatternTextBox.TabIndex = 0;
             // 
             // backdropFromMovieFolderCheckBox
             // 
             this.backdropFromMovieFolderCheckBox.AutoSize = true;
+            this.backdropFromMovieFolderCheckBox.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.backdropFromMovieFolderCheckBox.IgnoreSettingName = false;
-            this.backdropFromMovieFolderCheckBox.Location = new System.Drawing.Point(6, 80);
+            this.backdropFromMovieFolderCheckBox.Location = new System.Drawing.Point(127, 64);
             this.backdropFromMovieFolderCheckBox.Name = "backdropFromMovieFolderCheckBox";
             this.backdropFromMovieFolderCheckBox.Setting = null;
-            this.backdropFromMovieFolderCheckBox.Size = new System.Drawing.Size(231, 17);
+            this.backdropFromMovieFolderCheckBox.Size = new System.Drawing.Size(234, 17);
             this.backdropFromMovieFolderCheckBox.TabIndex = 4;
             this.backdropFromMovieFolderCheckBox.Text = "Search movie folders for backdrops named:";
             this.backdropFromMovieFolderCheckBox.UseVisualStyleBackColor = true;
             this.backdropFromMovieFolderCheckBox.CheckedChanged += new System.EventHandler(this.backdropFromMovieFolderCheckBox_CheckedChanged);
             // 
-            // helpButton1
-            // 
-            this.helpButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.helpButton1.BackgroundImage = global::MediaPortal.Plugins.MovingPictures.Properties.Resources.help;
-            this.helpButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.helpButton1.Location = new System.Drawing.Point(547, 13);
-            this.helpButton1.Margin = new System.Windows.Forms.Padding(0);
-            this.helpButton1.Name = "helpButton1";
-            this.helpButton1.Size = new System.Drawing.Size(23, 23);
-            this.helpButton1.TabIndex = 2;
-            this.helpButton1.UseVisualStyleBackColor = true;
-            // 
             // coverFromMovieFolderCheckBox
             // 
             this.coverFromMovieFolderCheckBox.AutoSize = true;
+            this.coverFromMovieFolderCheckBox.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.coverFromMovieFolderCheckBox.IgnoreSettingName = false;
-            this.coverFromMovieFolderCheckBox.Location = new System.Drawing.Point(6, 54);
+            this.coverFromMovieFolderCheckBox.Location = new System.Drawing.Point(127, 38);
             this.coverFromMovieFolderCheckBox.Name = "coverFromMovieFolderCheckBox";
             this.coverFromMovieFolderCheckBox.Setting = null;
-            this.coverFromMovieFolderCheckBox.Size = new System.Drawing.Size(223, 17);
+            this.coverFromMovieFolderCheckBox.Size = new System.Drawing.Size(229, 17);
             this.coverFromMovieFolderCheckBox.TabIndex = 1;
             this.coverFromMovieFolderCheckBox.Text = "Search movie folders for cover art named:";
             this.coverFromMovieFolderCheckBox.UseVisualStyleBackColor = true;
             this.coverFromMovieFolderCheckBox.CheckedChanged += new System.EventHandler(this.coverFromMovieFolderCheckBox_CheckedChanged);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Artwork:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Location = new System.Drawing.Point(120, 88);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(424, 3);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
             // ArtworkSettingsPane
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.groupBox);
-            this.MinimumSize = new System.Drawing.Size(400, 147);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.coverDataSources);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.backdropSourcesButton);
+            this.Controls.Add(this.coverFromMovieFolderCheckBox);
+            this.Controls.Add(this.coverCountTextBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.backdropFromMovieFolderCheckBox);
+            this.Controls.Add(this.coverPatternTextBox);
+            this.Controls.Add(this.backdropPatternTextBox);
+            this.MinimumSize = new System.Drawing.Size(550, 130);
             this.Name = "ArtworkSettingsPane";
-            this.Size = new System.Drawing.Size(576, 147);
+            this.Size = new System.Drawing.Size(555, 130);
             this.Load += new System.EventHandler(this.ArtworkSettingsPane_Load);
-            this.groupBox.ResumeLayout(false);
-            this.groupBox.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox;
         private Cornerstone.GUI.Controls.SettingCheckBox coverFromMovieFolderCheckBox;
         private Cornerstone.GUI.Controls.SettingsTextBox coverPatternTextBox;
-        private System.Windows.Forms.Button helpButton1;
         private Cornerstone.GUI.Controls.SettingCheckBox backdropFromMovieFolderCheckBox;
         private Cornerstone.GUI.Controls.SettingsTextBox backdropPatternTextBox;
         private System.Windows.Forms.Label label1;
         private Cornerstone.GUI.Controls.SettingsTextBox coverCountTextBox;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button coverDataSources;
         private System.Windows.Forms.Button backdropSourcesButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
--- MovingPictures/ConfigScreen/MovingPicturesConfig.Designer.cs
+++ MovingPictures/ConfigScreen/MovingPicturesConfig.Designer.cs
@@ -31,6 +31,7 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             this.movieImporterPane1 = new MediaPortal.Plugins.MovingPictures.ConfigScreen.MovieImporterPane();
             this.importPathsListView1 = new MediaPortal.Plugins.MovingPictures.ConfigScreen.ImportPathsPane();
             this.guiTab = new System.Windows.Forms.TabPage();
+            this.guiSettingsPane1 = new MediaPortal.Plugins.MovingPictures.ConfigScreen.GUISettingsPane();
             this.importerSettingsTab = new System.Windows.Forms.TabPage();
             this.importerSettingsPane1 = new MediaPortal.Plugins.MovingPictures.ConfigScreen.ImporterSettingsPane();
             this.artworkSettingsPane1 = new MediaPortal.Plugins.MovingPictures.ConfigScreen.ArtworkSettingsPane();
@@ -39,15 +40,13 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             this.mainTabControl.SuspendLayout();
             this.managerTab.SuspendLayout();
             this.importSettingsTab.SuspendLayout();
+            this.guiTab.SuspendLayout();
             this.importerSettingsTab.SuspendLayout();
             this.aboutTabPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainTabControl
             // 
-            this.mainTabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
             this.mainTabControl.Controls.Add(this.managerTab);
             this.mainTabControl.Controls.Add(this.importSettingsTab);
             this.mainTabControl.Controls.Add(this.guiTab);
@@ -56,16 +55,17 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             this.mainTabControl.Location = new System.Drawing.Point(12, 12);
             this.mainTabControl.Name = "mainTabControl";
             this.mainTabControl.SelectedIndex = 0;
-            this.mainTabControl.Size = new System.Drawing.Size(568, 499);
+            this.mainTabControl.Size = new System.Drawing.Size(568, 543);
             this.mainTabControl.TabIndex = 0;
             // 
             // managerTab
             // 
             this.managerTab.Controls.Add(this.movieManagerPane1);
+            this.managerTab.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.managerTab.Location = new System.Drawing.Point(4, 22);
             this.managerTab.Name = "managerTab";
             this.managerTab.Padding = new System.Windows.Forms.Padding(3);
-            this.managerTab.Size = new System.Drawing.Size(560, 559);
+            this.managerTab.Size = new System.Drawing.Size(560, 517);
             this.managerTab.TabIndex = 2;
             this.managerTab.Text = "Movie Manager";
             this.managerTab.UseVisualStyleBackColor = true;
@@ -75,7 +75,7 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             this.movieManagerPane1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.movieManagerPane1.Location = new System.Drawing.Point(3, 3);
             this.movieManagerPane1.Name = "movieManagerPane1";
-            this.movieManagerPane1.Size = new System.Drawing.Size(554, 553);
+            this.movieManagerPane1.Size = new System.Drawing.Size(554, 511);
             this.movieManagerPane1.TabIndex = 0;
             // 
             // importSettingsTab
@@ -85,7 +85,7 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             this.importSettingsTab.Location = new System.Drawing.Point(4, 22);
             this.importSettingsTab.Name = "importSettingsTab";
             this.importSettingsTab.Padding = new System.Windows.Forms.Padding(3);
-            this.importSettingsTab.Size = new System.Drawing.Size(560, 559);
+            this.importSettingsTab.Size = new System.Drawing.Size(560, 517);
             this.importSettingsTab.TabIndex = 0;
             this.importSettingsTab.Text = "Movie Importer";
             this.importSettingsTab.UseVisualStyleBackColor = true;
@@ -99,7 +99,7 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             this.movieImporterPane1.Location = new System.Drawing.Point(6, 190);
             this.movieImporterPane1.MinimumSize = new System.Drawing.Size(422, 250);
             this.movieImporterPane1.Name = "movieImporterPane1";
-            this.movieImporterPane1.Size = new System.Drawing.Size(548, 349);
+            this.movieImporterPane1.Size = new System.Drawing.Size(548, 321);
             this.movieImporterPane1.TabIndex = 1;
             // 
             // importPathsListView1
@@ -114,12 +114,24 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             // 
             // guiTab
             // 
+            this.guiTab.Controls.Add(this.guiSettingsPane1);
             this.guiTab.Location = new System.Drawing.Point(4, 22);
             this.guiTab.Name = "guiTab";
-            this.guiTab.Size = new System.Drawing.Size(560, 559);
+            this.guiTab.Padding = new System.Windows.Forms.Padding(3);
+            this.guiTab.Size = new System.Drawing.Size(560, 517);
             this.guiTab.TabIndex = 5;
             this.guiTab.Text = "GUI Settings";
             this.guiTab.UseVisualStyleBackColor = true;
+            // 
+            // guiSettingsPane1
+            // 
+            this.guiSettingsPane1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guiSettingsPane1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guiSettingsPane1.Location = new System.Drawing.Point(3, 3);
+            this.guiSettingsPane1.MinimumSize = new System.Drawing.Size(460, 100);
+            this.guiSettingsPane1.Name = "guiSettingsPane1";
+            this.guiSettingsPane1.Size = new System.Drawing.Size(554, 511);
+            this.guiSettingsPane1.TabIndex = 0;
             // 
             // importerSettingsTab
             // 
@@ -128,7 +140,7 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             this.importerSettingsTab.Location = new System.Drawing.Point(4, 22);
             this.importerSettingsTab.Name = "importerSettingsTab";
             this.importerSettingsTab.Padding = new System.Windows.Forms.Padding(3);
-            this.importerSettingsTab.Size = new System.Drawing.Size(560, 473);
+            this.importerSettingsTab.Size = new System.Drawing.Size(560, 517);
             this.importerSettingsTab.TabIndex = 3;
             this.importerSettingsTab.Text = "Importer Settings";
             this.importerSettingsTab.UseVisualStyleBackColor = true;
@@ -137,9 +149,9 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             // 
             this.importerSettingsPane1.Dock = System.Windows.Forms.DockStyle.Top;
             this.importerSettingsPane1.Location = new System.Drawing.Point(3, 3);
-            this.importerSettingsPane1.MinimumSize = new System.Drawing.Size(332, 304);
+            this.importerSettingsPane1.MinimumSize = new System.Drawing.Size(460, 250);
             this.importerSettingsPane1.Name = "importerSettingsPane1";
-            this.importerSettingsPane1.Size = new System.Drawing.Size(554, 304);
+            this.importerSettingsPane1.Size = new System.Drawing.Size(554, 250);
             this.importerSettingsPane1.TabIndex = 1;
             // 
             // artworkSettingsPane1
@@ -147,10 +159,10 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             this.artworkSettingsPane1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.artworkSettingsPane1.Location = new System.Drawing.Point(3, 317);
+            this.artworkSettingsPane1.Location = new System.Drawing.Point(3, 259);
             this.artworkSettingsPane1.MinimumSize = new System.Drawing.Size(400, 147);
             this.artworkSettingsPane1.Name = "artworkSettingsPane1";
-            this.artworkSettingsPane1.Size = new System.Drawing.Size(554, 153);
+            this.artworkSettingsPane1.Size = new System.Drawing.Size(554, 147);
             this.artworkSettingsPane1.TabIndex = 0;
             // 
             // aboutTabPage
@@ -159,7 +171,7 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             this.aboutTabPage.Location = new System.Drawing.Point(4, 22);
             this.aboutTabPage.Name = "aboutTabPage";
             this.aboutTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.aboutTabPage.Size = new System.Drawing.Size(560, 559);
+            this.aboutTabPage.Size = new System.Drawing.Size(560, 473);
             this.aboutTabPage.TabIndex = 6;
             this.aboutTabPage.Text = "About";
             this.aboutTabPage.UseVisualStyleBackColor = true;
@@ -169,15 +181,16 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             this.aboutPane1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.aboutPane1.Location = new System.Drawing.Point(3, 3);
             this.aboutPane1.Name = "aboutPane1";
-            this.aboutPane1.Size = new System.Drawing.Size(554, 553);
+            this.aboutPane1.Size = new System.Drawing.Size(554, 467);
             this.aboutPane1.TabIndex = 0;
             // 
             // MovingPicturesConfig
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 523);
+            this.ClientSize = new System.Drawing.Size(592, 567);
             this.Controls.Add(this.mainTabControl);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(600, 550);
@@ -190,6 +203,7 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             this.mainTabControl.ResumeLayout(false);
             this.managerTab.ResumeLayout(false);
             this.importSettingsTab.ResumeLayout(false);
+            this.guiTab.ResumeLayout(false);
             this.importerSettingsTab.ResumeLayout(false);
             this.aboutTabPage.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -210,5 +224,6 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
         private System.Windows.Forms.TabPage guiTab;
         private System.Windows.Forms.TabPage aboutTabPage;
         private AboutPane aboutPane1;
+        private GUISettingsPane guiSettingsPane1;
     }
 }
