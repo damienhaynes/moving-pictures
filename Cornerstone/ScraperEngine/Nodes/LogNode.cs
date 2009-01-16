a924bc327ecdb624262824c683c71f91007d2ce4
travistx <travistx@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2009-01-16 05:20:16 +0000

    Issue #293: Added a log element to the scripting engine Issue #281: Enhanced the Impawards script to: 1. Use the Google API instead of html screen scraping 2. Search on first 10 Alternate titles if the Title didn't match 3. Not download dupes of the first cover

---
+++ Cornerstone/ScraperEngine/Nodes/LogNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Threading;
+using NLog;
+
+namespace Cornerstone.ScraperEngine.Nodes {
+    [ScraperNode("log", LoadNameAttribute = false)]
+    public class LogNode : ScraperNode {
+        #region Properties
+
+        public LogLevel LogLevel {
+            get { return logLevel; }
+        } protected LogLevel logLevel;
+
+        public string Message {
+            get { return message; }
+        } protected String message;
+
+        #endregion
+
+        public LogNode(XmlNode xmlNode, bool debugMode)
+            : base(xmlNode, debugMode) {
+
+            try { logLevel = LogLevel.FromString(xmlNode.Attributes["LogLevel"].Value); }
+            catch (Exception e) {
+                if (e.GetType() == typeof(ThreadAbortException))
+                    throw e;
+
+                logLevel = LogLevel.Debug;
+            }
+
+            try { message = xmlNode.Attributes["Message"].Value; }
+            catch (Exception e) {
+                if (e.GetType() == typeof(ThreadAbortException))
+                    throw e;
+
+                logger.Error("Missing MESSAGE attribute on: " + xmlNode.OuterXml);
+                loadSuccess = false;
+                return;
+            }
+
+            loadSuccess = true;
+        }
+
+        public override void Execute(Dictionary<string, string> variables) {
+            logger.Log(this.LogLevel, parseString(variables, this.Message));
+        }
+    }
+}
