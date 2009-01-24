fc9c959581fb097e05ca3a9cf6bbbd47c7385141
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2009-01-24 22:17:37 +0000

    * Update from main branch for the OFDb script.
    * Fixed bug causing the change not to commit when switching the version of a script.

--- MovingPictures/Properties/AssemblyInfo.cs
+++ MovingPictures/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.6.5.418")]
+[assembly: AssemblyVersion("0.6.6.463")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("MovingPicturesUnitTest")]
