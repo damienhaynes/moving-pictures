11b2c95ee5fc4a3a69033859cb5b2481ebc586f5
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2009-01-24 22:18:57 +0000

    Tag for 0.6.6.

---
+++ 0.6.6/MovingPictures/Database/DBScriptInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cornerstone.Database;
+using Cornerstone.Database.Tables;
+using MediaPortal.Plugins.MovingPictures.DataProviders;
+
+namespace MediaPortal.Plugins.MovingPictures.Database {
+    [DBTableAttribute("scripts")]
+    public class DBScriptInfo: MovingPicturesDBTable {
+        #region Database Fields
+
+        [DBFieldAttribute]
+        public string Contents {
+            get { return contents; }
+
+            set {
+                contents = value;
+                commitNeeded = true;
+            }
+        } private string contents;
+
+        #endregion
+
+        public IScriptableMovieProvider Provider {
+            get {
+                if (provider == null) {
+                    Reload();
+                }
+
+                return provider;
+            }
+        } ScriptableProvider provider = null;
+
+        public void Reload() {
+            provider = new ScriptableProvider();
+            if (!provider.Load(Contents))
+                provider = null;
+        }
+
+        public override bool Equals(object obj) {
+            if (obj.GetType() != typeof(DBScriptInfo))
+                return base.Equals(obj);
+
+            return Provider.Equals(((DBScriptInfo)obj).Provider);
+        }
+
+        public override int GetHashCode() {
+            return Provider.GetHashCode();
+        }
+    }
+}
