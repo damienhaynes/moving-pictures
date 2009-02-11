f82c9642d95c657add2acaaec7e4a7ad7ed574c7
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2009-02-11 03:36:50 +0000

    Issue #340: Added the Blue3wide skin files created by Harley (but slightly tweaked) to SVN. Also added these files to the installer so that if the user chooses, Blue3wide skin support will be added with the installation.
    * Fixed bug rarely causing a crash when the plug-in tried to publish movie details when there was not one selected in the MovieBrowser.

--- MovingPictures/Properties/AssemblyInfo.cs
+++ MovingPictures/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.7.0.485")]
+[assembly: AssemblyVersion("0.7.1.495")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("MovingPicturesUnitTest")]
