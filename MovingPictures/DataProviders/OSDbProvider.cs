961c3ed6950230e4faf28be2af0c768ad4d30f3e
apondman <apondman@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2009-02-22 01:14:42 +0000

    * Added missing namespace to OSDbProvider * Small tweak to noise filter attribute value

--- MovingPictures/DataProviders/OSDbProvider.cs
+++ MovingPictures/DataProviders/OSDbProvider.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Reflection;
+using System.Threading;
 using MediaPortal.Plugins.MovingPictures.Database;
 using MediaPortal.Plugins.MovingPictures.SignatureBuilders;
 using MediaPortal.Plugins.MovingPictures.DataProviders.OpenSubtitles;
--- MovingPictures/MovingPicturesSettings.cs
+++ MovingPictures/MovingPicturesSettings.cs
@@ -35,7 +35,7 @@ namespace MediaPortal.Plugins.MovingPictures {
             Description = "A regular expression that removes common used keywords from the folder/filename.",
             Groups = "|Movie Importer|Tweaks|",
             Identifier = "importer_filter",
-            Default = "(\b(576p|720p|1080p|1080i|dircut|directors cut|dvdrip|dvdscreener|dvdscr|avchd|wmv|ntsc|pal|mpeg|dsr|hd|r5|dvd|dvdr|dvd5|dvd9|bd5|bd9|dts|ac3|bluray|blu-ray|hdtv|pdtv|stv|hddvd|xvid|divx|x264|dxva|(?-i)FESTIVAL|LIMITED|WS|FS|PROPER|REPACK|RERIP|REAL|RETAIL|EXTENDED|REMASTERED|UNRATED|CHRONO|THEATRICAL|DC|SE|UNCUT|INTERNAL|DUBBED|SUBBED)\b([-].+?$)?)")]
+            Default = @"(\b(576p|720p|1080p|1080i|dircut|directors cut|dvdrip|dvdscreener|dvdscr|avchd|wmv|ntsc|pal|mpeg|dsr|hd|r5|dvd|dvdr|dvd5|dvd9|bd5|bd9|dts|ac3|bluray|blu-ray|hdtv|pdtv|stv|hddvd|xvid|divx|x264|dxva|(?-i)FESTIVAL|LIMITED|WS|FS|PROPER|REPACK|RERIP|REAL|RETAIL|EXTENDED|REMASTERED|UNRATED|CHRONO|THEATRICAL|DC|SE|UNCUT|INTERNAL|DUBBED|SUBBED)\b([-].+?$)?)")]
         public string NoiseFilter {
             get { return _noiseFilter; }
             set {
