c662fdd807945db3c05ee6d1a5b8d6d7583d5bdf
apondman <apondman@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2009-03-29 18:37:22 +0000

    Issue #390: Added new "file" attribute to RetrieveNode. This will allow the scraping of local files.

--- Cornerstone/ScraperEngine/Nodes/RetrieveNode.cs
+++ Cornerstone/ScraperEngine/Nodes/RetrieveNode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading;
 using System.Xml;
@@ -13,6 +14,10 @@ namespace Cornerstone.ScraperEngine.Nodes {
         public string Url {
             get { return url; }
         } protected String url;
+
+        public string File {
+            get { return file; }
+        } protected String file;
 
         public int MaxRetries {
             get { return maxRetries; }
@@ -44,76 +49,92 @@ namespace Cornerstone.ScraperEngine.Nodes {
         public RetrieveNode(XmlNode xmlNode, bool debugMode)
             : base(xmlNode, debugMode) {
 
-            // try to grab the url
-            try { url = xmlNode.Attributes["url"].Value; }
-            catch (Exception e) {
-                if (e.GetType() == typeof(ThreadAbortException))
-                    throw e;
+            // Set default attribute valuess
+            userAgent = "Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)";
+            allowUnsafeHeader = false;
+            maxRetries = 5;
+            timeout = 5000;
+            timeoutIncrement = 2000;
 
-                logger.Error("Missing URL attribute on: " + xmlNode.OuterXml);
+            // Load attributes
+            foreach (XmlAttribute attr in xmlNode.Attributes) {
+                switch (attr.Name) {
+                    case "input":
+                        url = attr.Value;
+                        break;
+                    case "file":
+                        file = attr.Value;
+                        break;
+                    case "useragent":
+                        userAgent = attr.Value;
+                        break;
+                    case "allow_unsafe_header":
+                        try { allowUnsafeHeader = bool.Parse(attr.Value); }
+                        catch (Exception e) {
+                            if (e.GetType() == typeof(ThreadAbortException))
+                                throw e;
+                        }
+                        break;
+                    case "encoding":
+                        // grab encoding, if not specified it will try to set 
+                        // the encoding using information from the response header.
+                        try { encoding = Encoding.GetEncoding(attr.Value); }
+                        catch (Exception e) {
+                            if (e.GetType() == typeof(ThreadAbortException))
+                                throw e;
+                        }
+                        break;
+                    case "retries":
+                        try { maxRetries = int.Parse(attr.Value); }
+                        catch (Exception e) {
+                            if (e.GetType() == typeof(ThreadAbortException))
+                                throw e;
+                        }
+                        break;
+                    case "timeout":
+                        try { timeout = int.Parse(attr.Value); }
+                        catch (Exception e) {
+                            if (e.GetType() == typeof(ThreadAbortException))
+                                throw e;
+                        }
+                        break;
+                    case "timeout_increment":
+                        try { timeoutIncrement = int.Parse(attr.Value); }
+                        catch (Exception e) {
+                            if (e.GetType() == typeof(ThreadAbortException))
+                                throw e;                            
+                        }
+                        break;
+                }
+            }
+
+            // Validate URL / FILE attribute
+            if (url == null && file == null) {
+                logger.Error("Missing URL or FILE attribute on: " + xmlNode.OuterXml);
                 loadSuccess = false;
                 return;
             }
 
-            // grab user agent. if none specified use defaults
-            try { userAgent = xmlNode.Attributes["useragent"].Value; }
-            catch (Exception e) {
-                if (e.GetType() == typeof(ThreadAbortException))
-                    throw e;
-
-                userAgent = "Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)";
-            }
-
-            // grab user agent. if none specified use defaults
-            try { allowUnsafeHeader = bool.Parse(xmlNode.Attributes["allow_unsafe_header"].Value); }
-            catch (Exception e) {
-                if (e.GetType() == typeof(ThreadAbortException))
-                    throw e;
-
-                allowUnsafeHeader = false;
-            }
-
-            // grab encoding, if not specified it will try to set 
-            // the encoding using information from the response header.
-            try {
-                encoding = Encoding.GetEncoding(xmlNode.Attributes["encoding"].Value);
-            }
-            catch (Exception e) {
-                if (e.GetType() == typeof(ThreadAbortException))
-                    throw e;
-            }
-
-            // grab timeout and retry values. if none specified use defaults
-            try { maxRetries = int.Parse(xmlNode.Attributes["retries"].Value); }
-            catch (Exception e) {
-                if (e.GetType() == typeof(ThreadAbortException))
-                    throw e;
-
-                maxRetries = 5;
-            }
-
-            try { timeout = int.Parse(xmlNode.Attributes["timeout"].Value); }
-            catch (Exception e) {
-                if (e.GetType() == typeof(ThreadAbortException))
-                    throw e;
-
-                timeout = 5000;
-            }
-
-            try { timeoutIncrement = int.Parse(xmlNode.Attributes["timeout_increment"].Value); }
-            catch (Exception e) {
-                if (e.GetType() == typeof(ThreadAbortException))
-                    throw e;
-
-                timeoutIncrement = 2000;
-            }
         }
 
         public override void Execute(Dictionary<string, string> variables) {
             if (DebugMode) logger.Debug("executing retrieve: " + xmlNode.OuterXml);
 
-            string parsedUrl = parseString(variables, url);
             string parsedName = parseString(variables, name);
+            string stringData = string.Empty;
+
+            if (url != null)
+                stringData = RetrieveUrl(variables);
+            else
+                stringData = ReadFile(variables);
+
+            // Set variable
+            setVariable(variables, parsedName, stringData);
+        }
+
+        // Retrieves an URL
+        private string RetrieveUrl(Dictionary<string, string> variables) {
+            string parsedUrl = parseString(variables, url);
             string pageContents = string.Empty;
 
             if (DebugMode) logger.Debug("Retrieving URL: {0}", parsedUrl);
@@ -142,7 +163,6 @@ namespace Cornerstone.ScraperEngine.Nodes {
                     // save the contents of the page
                     pageContents = grabber.GetString();
                 }
-                setVariable(variables, parsedName, pageContents);
             }
             catch (Exception e) {
                 if (e is ThreadAbortException)
@@ -150,7 +170,32 @@ namespace Cornerstone.ScraperEngine.Nodes {
 
                 logger.Warn("Could not connect to " + parsedUrl, e);
             }
+            return pageContents;
         }
+
+        // Reads a file
+        private string ReadFile(Dictionary<string, string> variables) {
+            string parsedFile = parseString(variables, file);
+            string fileContents = string.Empty;
+
+            if (DebugMode) logger.Debug("Reading file: {0}", parsedFile);
+
+            try {
+                StreamReader streamReader = new StreamReader(parsedFile);
+                fileContents = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+            catch (Exception e) {
+                if (e is ThreadAbortException)
+                    throw e;
+
+                logger.Warn("Could not read file: " + parsedFile, e);
+            }
+
+            return fileContents;
+        }
+
+        
 
         #endregion
     }
