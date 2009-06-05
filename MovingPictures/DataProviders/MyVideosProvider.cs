6880cf13af98c868a4cc78f44e42b331263fd527
conrad.john <conrad.john@gmail.com>
2009-06-05 02:00:55 +0000

    Issue #165: Added support for pulling movie details from the MyVideos database built into MediaPortal. Work done by LRFalk01, thanks!

--- MovingPictures/DataProviders/MyVideosProvider.cs
+++ MovingPictures/DataProviders/MyVideosProvider.cs
@@ -8,6 +8,8 @@ using MediaPortal.Configuration;
 using System.Text.RegularExpressions;
 using System.IO;
 using System.Windows.Forms;
+using SQLite.NET;
+
 
 namespace MediaPortal.Plugins.MovingPictures.DataProviders {
     public class MyVideosProvider: IMovieProvider {
@@ -34,7 +36,7 @@ namespace MediaPortal.Plugins.MovingPictures.DataProviders {
         }
 
         public bool ProvidesMoviesDetails {
-            get { return false; }
+            get { return true; }
         }
 
         public bool ProvidesCoverArt {
@@ -46,11 +48,32 @@ namespace MediaPortal.Plugins.MovingPictures.DataProviders {
         }
 
         public List<DBMovieInfo> Get(MovieSignature movieSignature) {
-            throw new NotImplementedException();
+            List<DBMovieInfo> results = new List<DBMovieInfo>();
+            if (movieSignature == null)
+                return results;
+
+            string idMovie = getMovieID(movieSignature.File);
+            if (idMovie == String.Empty)
+                return results;
+
+            DBMovieInfo movie = this.getMovieInfo(idMovie);
+            if (movie == null)
+                return results;
+            
+            results.Add(movie);
+            return results;
         }
 
         public UpdateResults Update(DBMovieInfo movie) {
-            throw new NotImplementedException();
+            if (movie == null)
+                return UpdateResults.FAILED;
+
+            if (movie != null)
+            {
+                movie.CopyUpdatableValues(movie);
+                return UpdateResults.SUCCESS;
+            }
+            return UpdateResults.FAILED;
         }
 
         public bool GetArtwork(DBMovieInfo movie) {
@@ -72,5 +95,104 @@ namespace MediaPortal.Plugins.MovingPictures.DataProviders {
         }
 
         #endregion
+
+        /// <summary>
+        /// Method to find the file's unique id in the MyVideo's database
+        /// </summary>
+        /// <param name="fileName">Filename to look for in the MyVideo's database</param>
+        /// <returns>unique id as string</returns>
+        private string getMovieID(string fileName)
+        {
+            string idMovie = String.Empty;
+            fileName = fileName.Replace("'", "''");
+            try
+            {
+                SQLiteClient mp_db = new SQLiteClient(Config.GetFile(Config.Dir.Database, @"VideoDatabaseV5.db3"));
+                SQLiteResultSet results = mp_db.Execute("SELECT idMovie FROM files WHERE strFilename LIKE '\\" + fileName + "'");
+                idMovie = results.GetField(0, 0);
+                mp_db.Close();
+            }
+            catch
+            {
+            }
+            return idMovie;
+        }
+
+        private DBMovieInfo getMovieInfo(string idMovie)
+        {
+            DBMovieInfo movieRes = new DBMovieInfo();
+            try
+            {
+                SQLiteClient mp_db = new SQLiteClient(Config.GetFile(Config.Dir.Database, @"VideoDatabaseV5.db3"));
+                SQLiteResultSet sqlResults = mp_db.Execute("SELECT * FROM movieinfo WHERE idMovie LIKE '" + idMovie + "'");
+
+                SQLiteResultSet.Row sqlRow = sqlResults.GetRow(0);
+                System.Collections.Hashtable columns = sqlResults.ColumnIndices;
+
+                movieRes.Popularity = int.Parse(sqlResults.GetField(0, int.Parse(columns["strVotes"].ToString())));
+                movieRes.Runtime = int.Parse(sqlResults.GetField(0, int.Parse(columns["runtime"].ToString())));
+                movieRes.Score = float.Parse(sqlResults.GetField(0, int.Parse(columns["fRating"].ToString())));
+                movieRes.Year = int.Parse(sqlResults.GetField(0, int.Parse(columns["iYear"].ToString())));
+
+                string Title = sqlResults.GetField(0, int.Parse(columns["strTitle"].ToString()));
+                if (!Title.Contains("unknown"))
+                    movieRes.Title = Title;
+
+                string Certification = sqlResults.GetField(0, int.Parse(columns["mpaa"].ToString()));
+                if (!Certification.Contains("unknown"))
+                    movieRes.Certification = Certification;
+
+                string Tagline = sqlResults.GetField(0, int.Parse(columns["strTagLine"].ToString()));
+                if (!Tagline.Contains("unknown"))
+                    movieRes.Tagline = Tagline;
+
+                string Summary = sqlResults.GetField(0, int.Parse(columns["strPlotOutline"].ToString()));
+                if (!Summary.Contains("unknown"))
+                    movieRes.Summary = Summary;
+
+                string imdb_id = sqlResults.GetField(0, int.Parse(columns["IMDBID"].ToString()));
+                if (!imdb_id.Contains("unknown"))
+                    movieRes.ImdbID = imdb_id;
+
+                string genreMain = sqlResults.GetField(0, int.Parse(columns["strGenre"].ToString()));
+                if (!genreMain.Contains("unknown"))
+                {
+                    string[] genreSplit = genreMain.Split('/');
+                    foreach (string genre in genreSplit)
+                    {
+                        movieRes.Genres.Add(genre.Trim());
+                    }
+                }
+
+                string castMain = sqlResults.GetField(0, int.Parse(columns["strCast"].ToString()));
+                if (!castMain.Contains("unknown"))
+                {
+                    string[] castSplit = castMain.Split('\n');
+                    foreach (string cast in castSplit)
+                    {
+                        string castFinal = cast;
+                        if (cast.Contains(" as "))
+                            castFinal = cast.Remove(cast.IndexOf(" as "));
+                        movieRes.Actors.Add(castFinal.Trim());
+                    }
+                }
+
+                string idDirector = sqlResults.GetField(0, int.Parse(columns["idDirector"].ToString()));
+                if (!castMain.Contains("unknown"))
+                {
+                    SQLiteResultSet sqlDirector = mp_db.Execute("SELECT strActor FROM actors WHERE idActor LIKE '" + idDirector + "'");
+                    movieRes.Directors.Add(sqlDirector.GetField(0, 0));
+                }
+
+                mp_db.Close();
+            }
+            catch
+            {
+                return null;
+            }
+
+            return movieRes;
+        }
+
     }
 }
