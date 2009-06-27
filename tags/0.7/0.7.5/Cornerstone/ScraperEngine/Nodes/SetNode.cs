5a657829cb86d684b925526988aee67271ddf70f
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2009-06-27 18:07:14 +0000

    Tag for 0.7.5 release.

---
+++ tags/0.7/0.7.5/Cornerstone/ScraperEngine/Nodes/SetNode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Threading;
+
+namespace Cornerstone.ScraperEngine.Nodes {
+    [ScraperNode("set")]
+    public class SetNode: ScraperNode {
+        #region Properties
+
+        public string Value {
+            get { return value; }
+        } protected String value;
+
+        #endregion
+
+        #region Methods
+
+        public SetNode(XmlNode xmlNode, bool debugMode)
+            : base(xmlNode, debugMode) {
+
+            if (DebugMode) logger.Debug("executing set: " + xmlNode.OuterXml);
+            
+            // try to grab the value
+            try { value = xmlNode.Attributes["value"].Value; }
+            catch (Exception e) {
+              if (e.GetType() == typeof(ThreadAbortException))
+                throw e;
+
+              loadSuccess = false;
+            }
+            
+            // get the innervalue
+            string innerValue = xmlNode.InnerText.Trim();
+            
+            // Display an error if two values are set 
+            if (loadSuccess && !innerValue.Equals(String.Empty)) {
+              logger.Error("Ambiguous assignment on: " + xmlNode.OuterXml);
+              loadSuccess = false;
+              return;
+            }
+
+            // Display an error if no values are set
+            if (!loadSuccess && innerValue.Equals(String.Empty)) {
+              logger.Error("Missing VALUE attribute on: " + xmlNode.OuterXml);
+              return;
+            }
+
+            // Use the innerValue if we don't have a VALUE attribute
+            if (!loadSuccess && !innerValue.Equals(String.Empty)) {
+              loadSuccess = true;
+              value = innerValue;              
+            }
+
+        }
+
+        public override void Execute(Dictionary<string, string> variables) {
+            setVariable(variables, parseString(variables, Name), parseString(variables, value));
+        }
+
+        #endregion
+    }
+}
