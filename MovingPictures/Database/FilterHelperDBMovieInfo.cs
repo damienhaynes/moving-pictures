f3c14072f0913bf45ce1524e98b62ab0db70b15b
conrad.john <conrad.john@gmail.com>
2009-09-14 03:02:06 +0000

    Fixed bug causing a dynamic filter or category of type "year" to drop and recreate all filers and criteria on launch.

--- MovingPictures/Database/FilterHelperDBMovieInfo.cs
+++ MovingPictures/Database/FilterHelperDBMovieInfo.cs
@@ -46,7 +46,7 @@ namespace MediaPortal.Plugins.MovingPictures.Database {
                     continue;
 
                 try {
-                    int decade = (int.Parse(currSubNode.Filter.Criteria[1].Value.ToString()) / 10) - 10;
+                    int decade = int.Parse(currSubNode.Filter.Criteria[1].Value.ToString()) / 10;
                     if (!decades.Contains(decade))
                         toRemove.Add(currSubNode);
                     else
