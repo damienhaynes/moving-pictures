a1dbdfd18cc8a814bdc54fe4c78fd61813623d2e
conrad.john <conrad.john@gmail.com>
2010-01-07 06:56:21 +0000

    Issue #662 & Issue #162: Added new logic for artwork downloading to allow for resuming when the connection is lost mid-transfer. This should eliminate the corrupted image issue  (Issue #662). I also moved some of this logic to new classes which is a step in the right direction towards Issue #162. Both require additional work.
    I will probably polish things and do another commit for Issue #662 in the next few days. Regardless, feedback is DEFINATELY welcome. I'm looking for testing to verify this fixes the "corrupted images" issue, as well as a code review of the new logic and minor refactoring.

---
+++ MovingPictures/LocalMediaManagement/MovieResources/Backdrop.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using MediaPortal.Plugins.MovingPictures.Database;
+
+namespace MediaPortal.Plugins.MovingPictures.LocalMediaManagement.MovieResources {
+    public class Backdrop: ImageResource {
+        public override string Filename {
+            set {
+                base.Filename = value;
+
+                // build thumbnail filename
+                string thumbsFolder = MovingPicturesCore.Settings.BackdropThumbsFolder;
+                FileInfo file = new FileInfo(Filename);
+                ThumbFilename = thumbsFolder + "\\" + file.Name;
+            }
+        }
+
+        // genrate a filename for a backdrop. should be unique based on the source hash
+        private static string GenerateFilename(DBMovieInfo movie, string source) {
+            string artFolder = MovingPicturesCore.Settings.BackdropFolder;
+            string safeName = Utility.CreateFilename(movie.Title.Replace(' ', '.'));
+            return artFolder + "\\{" + safeName + "} [" + source.GetHashCode() + "].jpg";
+        }
+
+        public static Backdrop FromUrl(DBMovieInfo movie, string url, out ArtworkLoadStatus status) {
+            return FromUrl(movie, url, false, out status);
+        }
+
+        public static Backdrop FromUrl(DBMovieInfo movie, string url, bool ignoreRestrictions, out ArtworkLoadStatus status) {
+            int minWidth = MovingPicturesCore.Settings.MinimumBackdropWidth;
+            int minHeight = MovingPicturesCore.Settings.MinimumBackdropHeight;
+            bool redownloadBackdrops = MovingPicturesCore.Settings.RedownloadBackdrops; 
+            
+            Backdrop newBackdrop = new Backdrop();
+            newBackdrop.Filename = GenerateFilename(movie, url);
+
+            // if we already have a file for this movie from this URL
+            if (File.Exists(newBackdrop.Filename)) {
+                // if we are redownloading, just delete what we have
+                if (redownloadBackdrops) {
+                    try {
+                        File.Delete(newBackdrop.Filename);
+                        File.Delete(newBackdrop.ThumbFilename);
+                    }
+                    catch (Exception) { }
+                }
+                // otherwise return an "already loaded" failure
+                else {
+                    logger.Debug("Backdrop art for '" + movie.Title + "' [" + movie.ID + "] already exists from " + url + ".");
+                    status = ArtworkLoadStatus.ALREADY_LOADED;
+                    return null;
+                }
+            }
+
+            // try to grab the image if failed, exit
+            bool success = newBackdrop.Download(url);
+            if (!success) {
+                logger.Error("Failed retrieving backdrop for '" + movie.Title + "' [" + movie.ID + "] from " + url + ".");
+                status = ArtworkLoadStatus.FAILED;
+                return null;
+            }
+
+            // check resolution
+            System.Drawing.Image currImage = System.Drawing.Image.FromFile(newBackdrop.Filename);
+            if (!ignoreRestrictions && (currImage.Width < minWidth || currImage.Height < minHeight)) {
+                logger.Debug("Backdrop for '" + movie.Title + "' [" + movie.ID + "] failed minimum resolution requirements: " + url);
+                currImage.Dispose();
+                status = ArtworkLoadStatus.FAILED_RES_REQUIREMENTS;
+                return null;
+            }
+
+            status = ArtworkLoadStatus.SUCCESS;
+            return newBackdrop;
+        }
+
+        public static Cover FromFile(DBMovieInfo movie, string path) {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ MovingPictures/LocalMediaManagement/MovieResources/FileBasedResource.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using NLog;
+using System.Threading;
+
+namespace MediaPortal.Plugins.MovingPictures.LocalMediaManagement.MovieResources {
+    public class FileBasedResource {
+        public enum DownloadStatus {
+            SUCCESS, INCOMPLETE, FAILED
+        }
+
+        protected static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public virtual string Filename {
+            get { return _filename; }
+            set { _filename = value; }
+        }
+        private string _filename;
+
+        protected bool Download(string url) {
+            DownloadStatus status = DownloadStatus.INCOMPLETE;
+            long position = 0;
+            int attempts = 0;
+
+            while (status == DownloadStatus.INCOMPLETE) {
+                status = Download(url, position);
+                position = new FileInfo(_filename).Length;
+
+                attempts++;
+                if (attempts > 1 && status == DownloadStatus.INCOMPLETE)
+                    logger.Warn("Connection lost while downloading resource. Attempting to resume...");
+            }
+            
+            if (status == DownloadStatus.SUCCESS)
+                return true;
+            else
+                return false;
+        }
+
+        private DownloadStatus Download(string url, long startPosition) {
+            DownloadStatus rtn = DownloadStatus.SUCCESS;
+
+            HttpWebRequest request = null;
+            WebResponse response = null;
+            Stream webStream = null;
+            FileStream fileStream = null;
+
+            try {
+                // setup our file to be written to
+                if (startPosition == 0)
+                    fileStream = new FileStream(Filename, FileMode.Create, FileAccess.Write, FileShare.None);
+                else
+                    fileStream = new FileStream(Filename, FileMode.Append, FileAccess.Write, FileShare.None);
+
+                // open our connection
+                request = (HttpWebRequest)WebRequest.Create(url);
+                request.AddRange((int)startPosition);
+                response = request.GetResponse();
+                webStream = response.GetResponseStream();
+                
+                // setup our tracking variables for progress
+                int bytesRead = 0;
+                long totalBytesRead = 0;
+                long totalBytes = response.ContentLength + startPosition;
+
+                // download the file and progressively write it to disk
+                byte[] buffer = new byte[2048];
+                bytesRead = webStream.Read(buffer, 0, buffer.Length);
+                while (bytesRead > 0) {
+                    // write to our file
+                    fileStream.Write(buffer, 0, bytesRead);
+                    totalBytesRead = fileStream.Length;
+
+                    logger.Debug("Download progress: {2:0.0}% ({0:###,###,###} / {1:###,###,###} bytes)", totalBytesRead, totalBytes, 100.0 * totalBytesRead / totalBytes);
+
+                    // read the next stretch of data
+                    bytesRead = webStream.Read(buffer, 0, buffer.Length);
+                }
+
+                // if we failed delete the file and quit
+                if (fileStream.Length != totalBytes) {
+                    fileStream.Close();
+                    fileStream = null;
+
+                    //File.Delete(Filename);
+                    rtn = DownloadStatus.INCOMPLETE;
+                }
+
+            }
+            catch (Exception e) {
+                if (e is ThreadAbortException && fileStream != null) {
+                    fileStream.Close();
+                    fileStream = null;
+                    File.Delete(Filename);
+                }
+                else {
+                    logger.ErrorException("Unexpected error downloading file from: " + url, e);
+                }
+
+                rtn = DownloadStatus.FAILED;
+            }          
+
+            if (webStream != null) webStream.Close();
+            if (fileStream != null) fileStream.Close();
+            if (response != null) response.Close();
+            if (request != null) request.Abort();
+
+            return rtn;
+        }
+    }
+}
