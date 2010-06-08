8015bbd0296c22ae202c18f0abef1f7fb7d1535e
travistx <travistx@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2010-06-08 02:56:14 +0000

    Fixed issue with parental control PIN accepting non-numeric keys with non-US keyboard layouts.

--- branches/1.0/MovingPictures/ConfigScreen/GUISettingsPane.cs
+++ branches/1.0/MovingPictures/ConfigScreen/GUISettingsPane.cs
@@ -25,9 +25,6 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
         DBSetting dvdInsertedAction;
         DBSetting defaultView;
 
-        bool nonNumberEntered;
-        bool deleteEntered;
-
         MenuEditorPopup menuEditorPopup = null;
 
         public GUISettingsPane() {
@@ -209,45 +206,17 @@ namespace MediaPortal.Plugins.MovingPictures.ConfigScreen {
             menuEditorPopup.MenuTree.Menu = categoriesMenu;
         }
 
-        // Handle the KeyDown event to determine the type of character entered into the control.
-        private void passwordTextBox_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e) {
-            // Initialize the flag to false.
-            nonNumberEntered = false;
-            deleteEntered = false;
-
-            // Determine whether the keystroke is a number from the top of the keyboard.
-            if (e.KeyCode < Keys.D0 || e.KeyCode > Keys.D9) {
-                // Determine whether the keystroke is a number from the keypad.
-                if (e.KeyCode < Keys.NumPad0 || e.KeyCode > Keys.NumPad9) {
-                    // Determine whether the keystroke is a backspace.
-                    if (e.KeyCode != Keys.Back && e.KeyCode != Keys.Delete) {
-                        // A non-numerical keystroke was pressed.
-                        // Set the flag to true and evaluate in KeyPress event.
-                        nonNumberEntered = true;
-                    }
-                    else {
-                        deleteEntered = true;
-                    }
-                }
-            }
-            //If shift key was pressed, it's not a number.
-            if (Control.ModifierKeys == Keys.Shift) {
-                nonNumberEntered = true;
-            }
-        }
 
         // This event occurs after the KeyDown event and can be used to prevent
         // characters from entering the control.
         private void passwordTextBox_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e) {
-            // Check for the flag being set in the KeyDown event.
-            if (nonNumberEntered == true || (passwordTextBox.Text.Length == 4 && !deleteEntered)) {
-                // Stop the character from being entered into the control since it is non-numerical.
-                e.Handled = true;
+            if ((!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))) {
+                e.Handled = true; // suppress
 
                 Point pos = passwordTextBox.Location;
                 pos.Y += 25;
                 Help.ShowPopup(this, "Password must be a four digit number.", PointToScreen(pos));
-            }            
+            }
         }
 
         private void passwordTextBox_TextChanged(object sender, EventArgs e) {
