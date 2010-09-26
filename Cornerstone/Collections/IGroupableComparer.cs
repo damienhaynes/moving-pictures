29ac5c199968cc3c436448c885fe0c020d3c30f7
apondman <apondman@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2010-09-26 02:34:57 +0000

    added new collections and related classes to experimental/apondman branch

---
+++ Cornerstone/Collections/IGroupableComparer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Cornerstone.Collections {
+    
+    /// <summary>
+    /// Generic comparer that supports grouping
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IGroupableComparer<T> : IComparer<T> {
+
+        /// <summary>
+        /// Should return the label/description for the group this item will belong to when this comparer is used
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        string GetGroupLabel(T item);
+
+    }
+}
---
+++ Cornerstone/Collections/SortableObservableCollection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Cornerstone.Collections {
+
+    /// <summary>
+    /// ObservableCollection with added sorting
+    /// <inheritdoc />
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SortableObservableCollection<T> : ObservableCollection<T> {
+
+        /// <inheritdoc />
+        public virtual void Sort() {
+            this.Sort(0, Count, null);
+        }
+
+        /// <inheritdoc />
+        public virtual void Sort(IComparer<T> comparer) {
+            this.Sort(0, Count, comparer);
+        }
+
+        /// <inheritdoc />
+        public virtual void Sort(int index, int count, IComparer<T> comparer) {
+            (this.Items as List<T>).Sort(index, count, comparer);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+    }
+}
