d8ba97368b55ebed7411a2f8e40ec8371a886e9f
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2010-11-18 19:18:52 +0000

    tag for version 1.1.1

---
+++ tags/1.1/1.1.1/Cornerstone/ScraperEngine/Nodes/ActionNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Cornerstone.ScraperEngine.Nodes {
+    [ScraperNode("action")]
+    public class ActionNode: ScraperNode {
+
+
+        public ActionNode(XmlNode xmlNode, InternalScriptSettings settings)
+            : base(xmlNode, settings) {
+        }
+
+        public override void Execute(Dictionary<string, string> variables) {
+            executeChildren(variables);
+        }
+
+
+    }
+}
---
+++ tags/1.1/1.1.1/MovingPicturesSocialAPI/UI/Panels/SplashPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MovingPicturesSocialAPI.UI.Panels {
+    public partial class SplashPanel : UserControl {
+        public SplashPanel() {
+            InitializeComponent();
+        }
+    }
+}
