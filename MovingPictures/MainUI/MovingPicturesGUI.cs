10f8ef5b207eb4e299e694c796442d1326046800
apondman <apondman@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2010-12-13 23:21:16 +0000

    Additional fix for Issue #927

--- MovingPictures/MainUI/MovingPicturesGUI.cs
+++ MovingPictures/MainUI/MovingPicturesGUI.cs
@@ -147,7 +147,7 @@ namespace MediaPortal.Plugins.MovingPictures.MainUI {
         }
 
         private void ClearFocus() {
-            foreach (System.Windows.UIElement currControl in this.controlList) {
+            foreach (GUIControl currControl in this.controlList) {
                 if (currControl is GUIButtonControl) ((GUIButtonControl)currControl).Focus = false;
             }
         }
@@ -192,7 +192,7 @@ namespace MediaPortal.Plugins.MovingPictures.MainUI {
                 if (linesArray.Length > 3) dialog.SetLine(4, linesArray[3]);
                 dialog.SetDefaultToYes(defaultYes);
 
-                foreach (System.Windows.UIElement item in dialog.Children) {
+                foreach (GUIControl item in dialog.Children) {
                     if (item is GUIButtonControl) {
                         GUIButtonControl btn = (GUIButtonControl)item;
                         if (btn.GetID == 11 && !String.IsNullOrEmpty(yesLabel)) // Yes button
