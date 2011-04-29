2b2a306564ec0e8becd437b89b8347bcbcc293f7
conrad.john <conrad.john@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2011-04-29 19:23:17 +0000

    tag for 1.1.3

---
+++ tags/1.1/1.1.3/Cornerstone/GUI/Filtering/CriteriaInputField.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Cornerstone.GUI.Filtering {
+    public enum CriteriaInputType {
+        INT, REAL, STRING, COMBO
+    }
+
+    public class CriteriaInputField: UserControl {
+        public TextBox TextBox;
+        public ComboBox ComboBox;
+
+        public CriteriaInputType InputType {
+            get { return _inputType; }
+            set {
+                _inputType = value;
+                UpdateInputField();
+            }
+        } private CriteriaInputType _inputType = CriteriaInputType.STRING;
+
+        public CriteriaInputField() {
+            BorderStyle = BorderStyle.None;
+            Name = "CriteriaInputField";
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+
+            ComboBox = new ComboBox();
+            ComboBox.Dock = DockStyle.Fill;
+            ComboBox.AutoSize = true;
+            ComboBox.AutoCompleteMode = AutoCompleteMode.Suggest;
+            ComboBox.AutoCompleteSource = AutoCompleteSource.ListItems;
+            Controls.Add(ComboBox);
+
+            TextBox = new TextBox(); 
+            TextBox.Dock = DockStyle.Fill;
+            TextBox.AutoSize = true;
+            Controls.Add(TextBox);
+
+            UpdateInputField();
+        }
+
+        private void UpdateInputField() {
+            SuspendLayout();
+
+            switch (InputType) {
+                case CriteriaInputType.COMBO:
+                    ComboBox.Visible = true;
+                    TextBox.Visible = false;
+                    break;
+
+                case CriteriaInputType.INT:
+                case CriteriaInputType.REAL:
+                case CriteriaInputType.STRING:
+                    ComboBox.Visible = false;
+                    TextBox.Visible = true;
+                    break;
+            }
+
+            ResumeLayout(true);
+        }
+    }
+}
---
+++ tags/1.1/1.1.3/Cornerstone/GUI/ItemSelectionPopup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Cornerstone.GUI.Controls;
+using Cornerstone.Database.Tables;
+using Cornerstone.Database.CustomTypes;
+
+namespace Cornerstone.GUI {
+    public partial class ItemSelectionPopup : Form, IFieldDisplaySettingsOwner {
+
+        public enum ExitStatusEnum { OK, CANCEL }
+
+        public ItemSelectionPopup() {
+            InitializeComponent();
+        }
+
+        public bool ShowCancelButton {
+            get { return _showCancelButton; }
+            
+            set { 
+                _showCancelButton = value;
+                cancelButton.Visible = value;
+            }
+        } private bool _showCancelButton = true;
+
+        [Category("Cornerstone Settings")]
+        [Description("Manage the type of database table this control connects to and which fields should be displayed.")]
+        public FieldDisplaySettings FieldDisplaySettings {
+            get {
+                if (_fieldSettings == null) {
+                    _fieldSettings = new FieldDisplaySettings();
+                    _fieldSettings.Owner = this;
+                }
+
+                return _fieldSettings;
+            }
+
+            set {
+                _fieldSettings = value;
+                _fieldSettings.Owner = this;
+            }
+        } private FieldDisplaySettings _fieldSettings = null;
+
+        public DBObjectListEditor ItemList {
+            get {
+                return itemList;
+            }
+        }
+
+        public ExitStatusEnum ExitStatus {
+            get { return _exitStatus; }
+        } private ExitStatusEnum _exitStatus;
+
+        public void OnFieldPropertiesChanged() {
+            itemList.FieldDisplaySettings.Table = FieldDisplaySettings.Table;
+            itemList.FieldDisplaySettings.FieldProperties = FieldDisplaySettings.FieldProperties;
+        }
+
+        private void okButton_Click(object sender, EventArgs e) {
+            _exitStatus = ExitStatusEnum.OK;
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e) {
+            _exitStatus = ExitStatusEnum.CANCEL;
+            Close();
+        }
+
+    }
+}
