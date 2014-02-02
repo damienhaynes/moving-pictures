06e3d8753329c55e8d9b825f39264def0d7b3111
damien.haynes <damien.haynes@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2014-02-02 02:55:06 +0000

    Updated project to .NET 4.0 and support for MediaPortal 1.7+.
    Binaries are based on 1.7 Pre-release and may require changes before official release. I will keep an eye on this and make changes accordingly.

--- trunk/External/merge.bat
+++ trunk/External/merge.bat
@@ -1,13 +1,15 @@
 @echo off
 
 md tmp
-ilmerge /out:tmp\MovingPictures.dll MovingPictures.dll CookComputing.XmlRpcV2.dll FollwitApi.dll
+rem ilmerge /out:tmp\MovingPictures.dll MovingPictures.dll CookComputing.XmlRpcV2.dll FollwitApi.dll
+ilmerge /out:tmp\MovingPictures.dll MovingPictures.dll CookComputing.XmlRpcV2.dll FollwitApi.dll /target:dll /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319 /wildcards
 IF EXIST MovingPictures_UNMERGED.dll del MovingPictures_UNMERGED.dll
 ren MovingPictures.dll MovingPictures_UNMERGED.dll
 IF EXIST MovingPictures_UNMERGED.pdb del MovingPictures_UNMERGED.pdb
 ren MovingPictures.pdb MovingPictures_UNMERGED.pdb
 
-ilmerge /out:tmp\Cornerstone.dll Cornerstone.dll Lucene.Net.dll
+rem ilmerge /out:tmp\Cornerstone.dll Cornerstone.dll Lucene.Net.dll
+ilmerge /out:tmp\Cornerstone.dll Cornerstone.dll Lucene.Net.dll /target:dll /targetplatform:v4,C:\Windows\Microsoft.NET\Framework\v4.0.30319 /wildcards
 IF EXIST Cornerstone_UNMERGED.dll del Cornerstone_UNMERGED.dll
 ren Cornerstone.dll Cornerstone_UNMERGED.dll
 IF EXIST Cornerstone_UNMERGED.pdb del Cornerstone_UNMERGED.pdb
