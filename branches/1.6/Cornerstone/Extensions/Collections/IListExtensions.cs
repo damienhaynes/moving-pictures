3323d3aac5cffc8ea002e40740a446f731f3ede0
damien.haynes <damien.haynes@0b06b813-554b-0410-a70d-9dc1bb9c0dc0>
2014-02-02 02:09:57 +0000

    Created a branch for v1.6.x release. This will be the last compatible *major* release for MediaPortal 1.6.
    Going forward, we will only be able to support MediaPortal 1.7, this branch can be used to create any needed patches if required.

---
+++ branches/1.6/Cornerstone/Extensions/Collections/IListExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cornerstone.Extensions.Collections {
+
+    public static class IListExtensions {
+
+        static Random randomizer;
+
+        /// <summary>
+        /// Returns a random object from the IList collection or null if it is empty
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static T Random<T>(this IList<T> list) where T : class {
+            if (list.Count == 0)
+                return null;
+
+            if (randomizer == null)
+                randomizer = new Random();
+            
+            if (list.Count > 1)
+                return list[randomizer.Next(list.Count)];
+            else
+                return list[0];
+        }
+
+    }
+}
---
+++ branches/1.6/Cornerstone/GUI/Filtering/GenericCriteriaListPanel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using Cornerstone.Database.Tables;
+using Cornerstone.Database;
+
+namespace Cornerstone.GUI.Filtering {
+    public partial class GenericCriteriaListPanel<T> : UserControl, IGenericCriteriaListPanel
+        where T : DatabaseTable {
+
+        private CriteriaPanel<T> selectedItem = null;
+        private Dictionary<IGenericFilter,CriteriaPanel<T>> itemLookup = new Dictionary<IGenericFilter,CriteriaPanel<T>>();
+
+        [Browsable(false)]
+        public DatabaseManager DBManager {
+            get {
+                return _dbManager;
+            }
+            set {
+                _dbManager = value;
+            }
+        } private DatabaseManager _dbManager;
+
+        public IGenericFilter SelectedCriteria {
+            get {
+                if (selectedItem == null)
+                    return null;
+
+                return selectedItem.Criteria;
+            }
+        } 
+
+        public GenericCriteriaListPanel() {
+            InitializeComponent();
+        }
+
+        public void AddCriteria(IGenericFilter criteria) {
+            DBCriteria<T> newCriteria = (DBCriteria<T>)criteria;
+
+            CriteriaPanel<T> newPanel = new CriteriaPanel<T>();
+            listPanel.Controls.Add(newPanel);
+            newPanel.DBManager = DBManager;
+            newPanel.Selected += ItemSelected;
+            newPanel.Criteria = newCriteria;
+            itemLookup[newCriteria] = newPanel;
+        }
+
+        public void RemoveSelectedCriteria() {
+            if (selectedItem != null) {
+                CriteriaPanel<T> removedItem = selectedItem;
+                selectedItem = null;
+                listPanel.Controls.Remove(removedItem);
+            }
+        }
+
+        public void ClearCriteria() {
+            foreach (CriteriaPanel<T> currPanel in itemLookup.Values) 
+                listPanel.Controls.Remove(currPanel);
+
+            itemLookup.Clear();
+        }
+
+        private void ItemSelected(CriteriaPanel<T> obj) {
+            selectedItem = obj;
+        }
+    }
+
+    public interface IGenericCriteriaListPanel {
+        DatabaseManager DBManager { get; set; }
+
+        IGenericFilter SelectedCriteria { get; } 
+
+        void AddCriteria(IGenericFilter criteria);
+        void RemoveSelectedCriteria();
+        void ClearCriteria();
+    }
+}
